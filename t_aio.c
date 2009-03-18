e4082b168d46576fdba677105dc23289b80a1119
Stathis Kamperis <ekamperi@gmail.com>
2009-03-18 13:29:30 +0000

    Add aio test unit, even if we don't support aio operatos currently

---
+++ t_aio.c
@@ -0,0 +1,42 @@
+#include <aio.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	char mybuf[1000];
+	struct aiocb iocb;
+	int fd;
+
+	/* Open file to read from. */
+	fd = open(argv[0], O_RDONLY);
+	assert(fd >= 0);
+
+	/* Populate aiocb data structure. */
+	memset(&iocb, 0, sizeof(iocb));
+	iocb.aio_nbytes = sizeof(mybuf);
+	iocb.aio_fildes = fd;
+	iocb.aio_offset = 0;
+	iocb.aio_buf = mybuf;
+
+	/* Issue an asynchronous read request. */
+	assert (aio_read(&iocb) != -1);
+
+	/* Wait for request to complete. */
+	while (aio_error(&iocb) == EINPROGRESS)
+		/* VOID */ ;
+
+	/* Get the final status of request. */
+	assert(aio_return(&iocb) != -1);
+
+	/* Close file. */
+	close(fd);
+
+	return (EXIT_SUCCESS);
+}
