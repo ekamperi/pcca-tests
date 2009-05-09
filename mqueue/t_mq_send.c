a4c0a646c2aa7568bb745a240327bdf1ba251640
Stathis Kamperis <ekamperi@gmail.com>
2009-05-09 18:57:51 +0300

    Initial import of mq_send() test case

---
+++ mqueue/t_mq_send.c
@@ -0,0 +1,45 @@
+#include <assert.h>
+#include <errno.h>
+#include <mqueue.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define	MQNAME	"/t_mq_send"
+#ifndef MQ_PRIO_MAX
+#define MQ_PRIO_MAX	32768	/* XXX: linux specific */
+#endif
+
+int main(void)
+{
+	struct mq_attr attr;
+	mqd_t md;
+       	int rv;
+
+	/* Invalid message queue descriptor. */
+	rv = mq_send(-1, "", 0, 0);
+	assert (rv == -1 && errno == EBADF);
+
+	/* Priority is out of range (0...MQ_PRIO_MAX-1) */
+	md = mq_open(MQNAME, O_CREAT | O_WRONLY, 0700, NULL); 
+	assert(md != -1);
+
+	rv = mq_send(md, "foo", sizeof("foo"), 2*MQ_PRIO_MAX);
+	assert(rv == -1 && errno == EINVAL);
+
+	mq_close(md);
+
+	/* Message size is larger than maximum allowed message size */
+	/* attr.mq_flags = ? */
+	attr.mq_maxmsg = 1;
+	attr.mq_msgsize = 1;
+	md = mq_open(MQNAME, O_CREAT | O_WRONLY, 0700, &attr);
+	assert(md != -1);
+
+	char *p;
+	rv = mq_send(md, p, 10, /* priority */ 0);
+	assert(rv == -1 && errno == EMSGSIZE);
+
+	mq_close(md);
+
+	printf("passed\n");
+}
