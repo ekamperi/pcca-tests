9335f425ab14e5bb3f4066f6f825e54543d1af45
Stathis Kamperis <beket@dragonflybsd.org>
2009-05-16 11:52:01 +0300

    Clean t_mq_oc test

--- mqueue/t_mq_oc.c
+++ mqueue/t_mq_oc.c
@@ -5,18 +5,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define	MQNAME	"/tmqoc2"
+#define	MQNAME	"/tmqoc"
 
 int main(void)
 {
 	mqd_t md;
 
 	/* Create a message queue for write only with default parameters. */
-	md = mq_open(MQNAME, O_CREAT | O_WRONLY, 0777, NULL);
+	md = mq_open(MQNAME, O_CREAT | O_EXCL | O_WRONLY, 0777, NULL);
 	assert(md != -1);
 
 	/* Disassociate with message queue. */
 	assert(mq_close(md) != -1);
+
+	/* Remove the message queue from the system. */
+	assert(mq_unlink(MQNAME) != -1);
+
+	printf("passed\n");
 
 	return (EXIT_SUCCESS);
 }
