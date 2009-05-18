211ff95239cfdd9f539e4f2b62dd1a5a6d30eac7
Stathis Kamperis <beket@dragonflybsd.org>
2009-05-18 12:58:55 +0300

    Try to receive a message from a write only queue.
    Current implementation lets you mq_open() a O_WRONLY queue
    in O_RDWR mode. I would expect to fail with EACCES (if I
    interpret the standard right). At least when you actually
    try to mq_receive() from the write only queue it fails,
    but with EPERM which deviates from standard as well.

--- mqueue/t_mq_open.c
+++ mqueue/t_mq_open.c
@@ -6,12 +6,12 @@
 #include <stdlib.h>
 #include <string.h>	/* memset() */
 
-#define	MQNAME	"/t_mq_open1"
+#define	MQNAME	"/t_mq_open40121"
 #define	PATH_MAX 1024	/* XXX */
 
 int main(void)
 {
-	mqd_t md;
+	mqd_t md, md2;
 
 	/* O_CREAT is not set and the named mqueue doesn't exist. */
 	md = mq_open("nonexistentmqueuename", O_WRONLY, 0700, NULL);
@@ -35,11 +35,18 @@ int main(void)
 	mq_unlink(MQNAME);
 
 	/* Try to open a read only message queue for write. */
-	md = mq_open(MQNAME, O_CREAT | O_EXCL | O_RDONLY, 0700, NULL);
+	md = mq_open(MQNAME, O_CREAT | O_EXCL | O_WRONLY, 0700, NULL);
 	assert(md != -1);
 
-	assert(mq_open(MQNAME, O_RDWR) == -1 && errno == EACCES);
+	md2 = mq_open(MQNAME, O_CREAT | O_RDWR);
+	assert(md2 != -1);
+
+	/* Receive message. */
+	char msg[8192];
+	assert(mq_receive(md, msg, sizeof(msg), NULL) == -1 && errno == EPERM);
+
 	mq_close(md);
+	mq_close(md2);
 	mq_unlink(MQNAME);
 
 	printf("passed\n");
