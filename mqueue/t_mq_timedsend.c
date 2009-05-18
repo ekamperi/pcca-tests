91f9d0b70944b127e69253aab2f307ca7f74a954
Stathis Kamperis <ekamperi@gmail.com>
2009-05-19 01:52:40 +0300

    Tweak comments; no functional change

--- mqueue/t_mq_timedsend.c
+++ mqueue/t_mq_timedsend.c
@@ -20,22 +20,21 @@ int main(void)
 	 * (we want to emulate a full queue).
 	 */
 	memset(&attr, 0, sizeof(attr));
-	attr.mq_maxmsg = 1;	/* Maximum number of messages */
-	attr.mq_msgsize = 1024;	/* Maximum message size */
+	attr.mq_maxmsg = 1;	/* Maximum number of messages. */
+	attr.mq_msgsize = 1024;	/* Maximum message size. */
 
 	md = mq_open(MQNAME, O_CREAT | O_EXCL | O_WRONLY, 0700, &attr);
 	assert(md != -1);
 
-	/* */
+	/* Send a message to fill the queue. */
 	assert(mq_send(md, "foo", sizeof("foo"), /* priority */ 0) != -1);
 
-	/* */
+	/* `0' seconds since 00:00:00 GMT 1 January 1970 */
 	abs_timeout.tv_sec = 0;
 	abs_timeout.tv_nsec = 0;
 
-	/* */
-	assert(mq_timedsend(md, "foo", sizeof("foo"),
-			    /* priority */ 0,
+	/* Try to send a second message to the queue with an expired timeout. */
+	assert(mq_timedsend(md, "foo", sizeof("foo"), /* priority */ 0,
 			    &abs_timeout) == -1 && errno == ETIMEDOUT);
 
 	mq_close(md);
