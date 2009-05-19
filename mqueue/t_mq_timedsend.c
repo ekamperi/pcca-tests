5d7c09a53ba52c76ae4c69b6859766a0b6d325e4
Stathis Kamperis <ekamperi@gmail.com>
2009-05-19 19:29:28 +0300

    Finish mq_timedsend() test case

--- mqueue/t_mq_timedsend.c
+++ mqueue/t_mq_timedsend.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <string.h>	/* memset() */
 #include <time.h>
+#include <unistd.h>	/* fork() */
+#include <sys/wait.h>
 
 #define	MQNAME	"/tmqts2"
 
@@ -13,7 +15,8 @@ int main(void)
 {
 	struct mq_attr attr;
 	struct timespec abs_timeout;
-	mqd_t md;
+	mqd_t md, md2;
+	pid_t pid;
 
 	/*
 	 * Create a message queue for write only with capacity `1'
@@ -35,20 +38,52 @@ int main(void)
 
 	/* Try to send a second message to the queue with an expired timeout. */
 	assert(mq_timedsend(md, "foo", sizeof("foo"), /* priority */ 0,
-			    &abs_timeout) == -1 && errno == ETIMEDOUT);
-
+		&abs_timeout) == -1 && errno == ETIMEDOUT);
 
 	/* Try to send a second message to the queue with a non expired timeout. */
 	abs_timeout.tv_sec = time(NULL) + 5;
-
+	abs_timeout.tv_nsec = 0;
 
 	assert(mq_timedsend(md, "foo", sizeof("foo"), /* priority */ 0,
-			    &abs_timeout) == -1);
+		&abs_timeout) == -1 && errno == ETIMEDOUT);
 
-	mq_close(md);
-	mq_unlink(MQNAME);
+	/*
+	 * Fork and let parent block on mq_timedsend(). In the meantime,
+	 * child receives the existing message from the queue, making room
+	 * for parent to send her!
+	 */
+	pid = fork();
+	assert(pid != -1);
 
-	printf("passed\n");
+	if (pid == 0) {
+		/* We are inside the child. */
+		md2 = mq_open(MQNAME, O_RDONLY);
+		assert(md2 != -2);
+
+		char msg_recvd[8192];	/* Implementation defined. */
+		assert(mq_receive(md2, msg_recvd, sizeof(msg_recvd),
+			/* priority */ NULL) != -1);
+
+		/* Disassociate from message queue. */
+		mq_close(md2);
+	} else {
+		/* We are inside the parent. */
+		/* Make sure the child has completed. */
+		int status;
+		wait(&status);
+
+		/* This shouldn't block now, since queue isn't full. */
+		assert(mq_timedsend(md, "foo", sizeof("foo"), /* priority */ 0,
+			&abs_timeout) != -1);
+
+		/* Disassociate from message queue. */
+		mq_close(md);
+
+		/* Remove message queue from the system. */
+		mq_unlink(MQNAME);
+
+		printf("passed\n");
+	}
 
 	return (EXIT_SUCCESS);
 }
