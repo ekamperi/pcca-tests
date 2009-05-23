194b26d976a0d8a785df113844d1ec23fd0b20ae
Stathis Kamperis <beket@dragonflybsd.org>
2009-05-23 11:27:51 +0300

    Add more stress tests

--- mqueue/t_mq_stress.sh
+++ mqueue/t_mq_stress.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 cnt=0
-while [ $cnt -lt 100000 ];
+while [ $cnt -lt 20000 ];
 do
+    ./t_mq_close
     ./t_mq_open
-    let cnt=cnt+1
+    ./t_mq_openclose
+    ./t_mq_parent_child
+    ./t_mq_parent_multmsg
+    ./t_mq_send
+    ./t_mq_setattr
+    ((cnt=$cnt+1))
 done
