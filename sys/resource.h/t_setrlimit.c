b72f3494c2745847cf4d61284b82c523babde447
Stathis Kamperis <beket@dragonflybsd.org>
2009-05-25 19:38:40 +0300

    t_setrlimit: Add test when new soft limit is larger than hard limit.

--- sys/resource.h/t_setrlimit.c
+++ sys/resource.h/t_setrlimit.c
@@ -13,6 +13,43 @@ int main(void)
 	assert(setrlimit(-112233, &rlm) == -1 && errno == EINVAL);
 	assert(setrlimit( 112233, &rlm) == -1 && errno == EINVAL);
 
+#if 0
+	/*
+	 * Try setting a soft limit larger than hard limit.
+	 * It seems that if DragonFly encounters a soft limit larger than hard
+	 * limit, it enforces the former to be as large as the latter. Rather
+	 * than returning EINVAL as POSIX mandates. On top of that it fails
+	 * later with EPERM because we aren't privileged enough to increase our
+	 * limits.
+	 * See kern/kern_plimit.c:355.
+	 */
+	/* Zero out structure. */
+	memset(&rlm, 0, sizeof(struct rlimit));
+
+	/* Get current stack size limit. */
+	assert(getrlimit(RLIMIT_STACK, &rlm) == 0);
+
+	/* Make sure we don't overflow. */
+	if (rlm.rlim_max + 1 > rlm.rlim_max) {
+		rlm.rlim_cur = rlm.rlim_max + 1;
+
+		fprintf(stderr, "soft: %lld\thard: %lld\n",
+		    rlm.rlim_cur, rlm.rlim_max);
+
+		if (setrlimit(RLIMIT_STACK, &rlm) == -1) {
+			perror("setrlimit");
+			fprintf(stderr, "It should fail with EINVAL\n");
+		}
+
+		struct rlimit newrlm;
+		assert(getrlimit(RLIMIT_STACK, &newrlm) == 0);
+		fprintf(stderr, "soft: %lld\thard: %lld\n",
+		    newrlm.rlim_cur, newrlm.rlim_max);
+	} else {
+		fprintf(stderr, "Skipping check.\n");
+	}
+#endif
+
 	/* Try increasing maximum limit value without being super-user. */
 	/* Zero out structure. */
 	memset(&rlm, 0, sizeof(struct rlimit));
