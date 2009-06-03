22f2e8f195288cf26b2823c5c312172e25ab0940
Stathis Kamperis <ekamperi@gmail.com>
2009-06-04 02:40:00 +0300

    Add test case for access.

---
+++ unistd.h/t_access.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	/* We expect our binary to exist, while we are running. */
+	assert(access(argv[0], F_OK) == 0);
+
+	/* Spy /root */
+	assert(access("/root", R_OK | W_OK | X_OK | F_OK) == -1 &&
+	       errno == EACCES);
+
+	/* Probe non existent paths. */
+	assert(access("", F_OK) == -1 && errno == ENOENT);
+	assert(access("/this/really/shouln/t/exist", F_OK) == -1 &&
+	       errno == ENOENT);
+
+	/* This is optional. */
+	assert(access(argv[0], ~(R_OK | W_OK | X_OK | F_OK)) == -1 && errno == EINVAL);
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
