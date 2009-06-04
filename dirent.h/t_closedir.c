ef9b26092a058dc47f970c3f237d1345a772ed8f
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-04 21:51:52 +0300

    t_closedir: gracefully handle EBADF failure.

--- dirent.h/t_closedir.c
+++ dirent.h/t_closedir.c
@@ -1,12 +1,27 @@
 #include <assert.h>
 #include <dirent.h>
 #include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Function prototypes. */
+static void myhandler(int );
 
 int main(void)
 {
 	DIR *dp;
+
+	/*
+	 * XXX: The following test must always be the last one to run.
+	 *
+	 * POSIX suggests that if the supplied argument of closedir(3) doesn't
+	 * refer to an open directory stream, EBADF _may_ be returned. Some
+	 * operating systems don't and the test case dies with segmentation
+	 * fault. Catch SIGSEGV , because otherwise the false impression of
+	 * the test case being flawed is given to the user.
+	 */
+	signal(SIGSEGV, myhandler);
 
 	/* Bad directory stream. */
 	assert(closedir(NULL) == 0 && errno == EBADF);
@@ -14,4 +29,14 @@ int main(void)
 	printf("passed\n");
 
 	return (EXIT_SUCCESS);
+}
+
+static void
+myhandler(int sig)
+{
+	/* Ignore subsequent occurences of the signal. */
+	signal(sig, SIG_IGN);
+
+	printf("passed (EBADF check was skipped)\n");
+	exit(EXIT_SUCCESS);
 }
