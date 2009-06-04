d56a3f999e47d7dcbb253d829965042816fea285
Stathis Kamperis <ekamperi@gmail.com>
2009-06-04 04:01:24 +0300

    Add test case for readlink.

---
+++ unistd.h/t_readlink.c
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	/* memset() */
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	char buf[4096];
+
+	/* We are not a symbolic link. */
+	assert(readlink(argv[0], buf, sizeof(buf) - 1) == -1 && errno == EINVAL);
+
+	/* Provide invalid path to link. */
+	assert(readlink("", buf, sizeof(buf) - 1) == -1 && errno == ENOENT);
+	assert(readlink("/this/really/shouldn/t/exist",
+			buf, sizeof(buf) - 1 == -1 && errno == ENOENT));
+
+	/* XXX: Exceed maximum path length. */
+	char *p;
+	p = malloc(32768);
+	assert(p != NULL);
+	memset(p, 0xff, 32768);
+	assert(readlink(p, buf, sizeof(buf) - 1 == -1 && errno == ENAMETOOLONG));
+	free(p);
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
