f2b6c016fe4b37c9aafb502016183f2d4a849ff2
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-04 22:51:22 +0300

    t_setpriority: Disable non-standard check.

--- sys/resource.h/t_setpriority.c
+++ sys/resource.h/t_setpriority.c
@@ -7,8 +7,6 @@
 
 int main(void)
 {
-	pid_t pid;
-
 	/* Invalid `which' value. */
 	assert(setpriority(-112233, 1, /* nice */ 0) == -1 && errno == EINVAL);
 	assert(setpriority( 112233, 1, /* nice */ 0) == -1 && errno == EINVAL);
@@ -21,11 +19,18 @@ int main(void)
         assert(setpriority(PRIO_PROCESS, 1, /* nice */ 0) == -1
 	    && errno == EPERM);
 
-	/* Be bad and try to lower our nice value, without having privilege. */
+#if 0
+	/*
+	 * Be bad and try to lower our nice value, without having privilege.
+	 * POSIX suggests in this matter, so we can't expect one behaviour.
+	 * e.g, the OS may renice up to the minimum allowed nice value.
+	 */
+	pid_t pid;
 	pid = getpid();
 
 	assert(setpriority(PRIO_PROCESS, pid, /* nice */ -10) == -1
 	    && errno == EACCES);
+#endif
 
 	printf("passed\n");
 
