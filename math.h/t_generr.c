4b213dfc0ec0e02e05a4dd88b69763858affec21
Stathis Kamperis <ekamperi@gmail.com>
2009-06-05 03:43:33 +0300

    t_generr: Fix typo.

--- math.h/t_generr.c
+++ math.h/t_generr.c
@@ -188,7 +188,7 @@ int main(void)
 
 		if (pid == 0) {
 			/* We are inside the child. */
-			printf("%s ", te->desc);
+			printf("%s: ", te->desc);
 			fflush(NULL);
 
 			/* Run the actual test. */
