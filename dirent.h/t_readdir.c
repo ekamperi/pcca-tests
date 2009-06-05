59842c1d78b3ae641aac4ab2a8ba87db9ddbd353
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-05 23:14:32 +0300

    Add simple test case for readdir.

---
+++ dirent.h/t_readdir.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	/* memset() */
+
+int main(int argc, char *argv[])
+{
+	DIR *dirp;
+	struct dirent *dp;
+	int found;
+
+	/* Open directory stream. */
+	dirp = opendir(".");
+	assert(dirp != NULL);
+
+	/* Iterate through the stream and make sure that . and .. are found. */
+	found = 0;
+	while ((dp = readdir(dirp)) != NULL) {
+		if (strcmp(dp->d_name, ".") == 0)
+			found++;
+		if (strcmp(dp->d_name, "..") == 0)
+			found++;
+	}
+
+	assert(found == 2);
+
+	assert(closedir(dirp) == 0);
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
