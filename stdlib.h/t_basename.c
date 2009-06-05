ec58c0de42a4ff647f16108a663f91b4283bce16
Stathis Kamperis <ekamperi@gmail.com>
2009-06-05 04:29:16 +0300

    t_basename: String literals aren't writeable.

--- stdlib.h/t_basename.c
+++ stdlib.h/t_basename.c
@@ -4,21 +4,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define	BUFSIZE	1024
+
 int main(void)
 {
-	const char *str[][2] = {{ "/usr/lib", "lib"},
-				{ "/usr/", "usr"},
-				{ "/", "/"},
-				{ "///", "/"},
-				{ "//usr//lib//", "lib"},
-				{ "", "."},
-				{ NULL, "."}};
+	char buf[BUFSIZE + 1];	/* Reserve space for '\0' */
 
-	assert(strcmp(basename(str[0][0]), str[0][1]) == 0);
-	assert(strcmp(basename(str[1][0]), str[1][1]) == 0);
-        assert(strcmp(basename(str[2][0]), str[2][1]) == 0);
-        assert(strcmp(basename(str[3][0]), str[3][1]) == 0);
-        assert(strcmp(basename(str[4][0]), str[4][1]) == 0);
+	/* Zero out buffer. */
+	memset(buf, 0, BUFSIZE + 1);
+
+	/*
+	 * POSIX allows basename() to modify the string pointed to by path.
+	 * Therefore, avoid passing string literals, as they are non-writeable.
+	 */
+	strncpy(buf, "/usr/lib", BUFSIZE);
+	assert(strcmp(basename(buf), "lib") == 0);
+
+	strncpy(buf, "/usr/", BUFSIZE);
+	assert(strcmp(basename(buf), "usr") == 0);
+
+	strncpy(buf, "/", BUFSIZE);
+        assert(strcmp(basename(buf), "/") == 0);
+
+	strncpy(buf, "///", BUFSIZE);
+        assert(strcmp(basename(buf), "/") == 0);
+
+	strncpy(buf, "//usr//lib//", BUFSIZE);
+        assert(strcmp(basename(buf), "lib") == 0);
+
+	strncpy(buf, "", BUFSIZE);
+	assert(strcmp(basename(buf), ".") == 0);
+
+	assert(strcmp(basename(NULL), ".") == 0);
 
 	printf("passed\n");
 
