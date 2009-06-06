3d33541dd76867f10b15e20a2419b21d438b2665
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-06 19:53:56 +0300

    build-tests.sh: Add option parsing.

--- build-tests.sh
+++ build-tests.sh
@@ -1,16 +1,74 @@
 #!/bin/sh
 
-for dir in `find . -name .git -prune -o -type d -a ! -name . -print`
-do
-    OLDPWD=`pwd`
+usage()
+{
+    printf "Usage: %s: [-c] [-b] [-r]\n" \
+        $(basename $0) >&2
+    printf -- "-c\tClean stale files from previous builds.\n"
+    printf -- "-b\tBuild tests from sources.\n"
+    printf -- "-r\tRun tests.\n"
+    printf -- "-h\tPrint this help message.\n"
+    printf    "At least one of the above options must be specified.\n"
+    exit 1
+}
 
-    cd "$dir"
-
-    if [ -e "Makefile" ]
+runtests()
+{
+    if [ ! -e "tlreader" ]
     then
-	make clean
-	make
+	cc tleader.c -o tlreader
     fi
 
-    cd "$OLDPWD"
+    find . -name .git -prune -o -type d -a ! -name . -exec ./tleader {} \;
+}
+
+buildtests()
+{
+    for dir in `find . -name .git -prune -o -type d -a ! -name . -print`
+    do
+	OLDPWD=`pwd`
+
+	cd "$dir"
+
+	if [ -e "Makefile" ]
+	then
+	    if [ ! -z "$clean" ]
+	    then
+		make clean
+	    fi
+	    make
+	fi
+
+	cd "$OLDPWD"
+    done
+}
+
+# Parse user supplied arguments
+while getopts "cbrh" f
+do
+    case $f in
+        c)
+            clean=$f
+            ;;
+	b)
+	    build=$f
+	    ;;
+	r)
+	    run=$f
+	    ;;
+        h)
+            usage
+            ;;
+        \?)
+            usage
+            ;;
+    esac
 done
+shift `expr $OPTIND - 1`
+
+# At least one of -c, -b, -r options must be set.
+[ -z "$clean" ] && [ -z "$build"] && [ -z "$run" ] && usage
+
+# Fire!
+[ ! -z "$build" ] && buildtests
+[ ! -z "$run" ] && runtests
