90982e72c50b2e0a0c6e8c9200202e1c75bf43b0
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-16 07:40:31 +0300

    t_carg: Make sure that result is in [-pi, +pi] interval.

--- complex.h/t_carg.c
+++ complex.h/t_carg.c
@@ -7,8 +7,17 @@
 int main(void)
 {
 	double complex z1 = 1.0 + 1.0 * I;
+	double complex z2;
+	double y;
 
 	assert(fabs(carg(z1) - M_PI_4) < 0.001);
+
+	/* Make sure that the return value is in the [-pi, +pi] interval. */
+	for (y = -2.0; y < 2.0; y += 0.05) {
+		z2 = 1.0 + y * I;
+		assert(carg(z2) <= M_PI && carg(z2) >= -M_PI);
+	}
+
 
 	printf("passed\n");
 
