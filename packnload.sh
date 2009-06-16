0e832b63b36c61201ec9ac1ccc1a6b74bd7b4528
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-16 10:22:29 +0300

    packnload.sh: Print informative messages.

--- packnload.sh
+++ packnload.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 filename=pcca-tests-`date '+%d-%m-%y'`.tar.gz
+
+printf "Creating git archive with name $filename... "
 git archive --format=tar HEAD | gzip > $filename
+printf "DONE\n"
+
+printf "Uploading git archive to leaf.dragonflybsd.org/~public_html... "
 scp $filename beket@leaf.dragonflybsd.org:~/public_html
+printf "DONE\n"
