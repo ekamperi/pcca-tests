5a12fce80efe324601a31bcd3431b36fab4749f8
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-16 10:23:05 +0300

    packnload.sh: Capitalize variables.

--- packnload.sh
+++ packnload.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-filename=pcca-tests-`date '+%d-%m-%y'`.tar.gz
+FILENAME=pcca-tests-`date '+%d-%m-%y'`.tar.gz
 
-printf "Creating git archive with name $filename... "
-git archive --format=tar HEAD | gzip > $filename
+printf "Creating git archive with name $FILENAME... "
+git archive --format=tar HEAD | gzip > $FILENAME
 printf "DONE\n"
 
 printf "Uploading git archive to leaf.dragonflybsd.org/~public_html... "
-scp $filename beket@leaf.dragonflybsd.org:~/public_html
+scp $FILENAME beket@leaf.dragonflybsd.org:~/public_html
 printf "DONE\n"
