93bcd4500212365b3bd0b30d8013569643344d5c
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-18 15:55:15 +0300

    Add test case for csqrt.

---
+++ complex.h/t_csqrt.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <complex.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tentry {
+	double complex x;	/* Input */
+	double complex y;	/* Cos output */
+} ttable[] = {
+	{ 7.123 + 1.456*I, 2.682656 + 0.271373*I },
+	{-3.505 + 1.210*I, 0.318576 + 1.899076*I },
+	{ 4.587 - 0.667*I, 2.147352 - 0.155308*I },
+	{-1.101 - 1.552*I, 0.633193 - 1.225534*I }
+};
+
+int main(void)
+{
+	size_t i;
+
+	for (i = 0; i < sizeof(ttable) / sizeof(struct tentry); i++) {
+		/* Compare real parts. */
+		assert(fabs(creal(csqrt(ttable[i].x)) - creal(ttable[i].y))
+		    < 0.001);
+
+		/* Compare imaginary parts. */
+		assert(fabs(cimag(csqrt(ttable[i].x)) - cimag(ttable[i].y))
+		    < 0.001);
+	}
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
