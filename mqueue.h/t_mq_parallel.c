7087085ba451fca0fc23a3d4be5049d743446339
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-20 20:37:53 +0300

    t_mq_parallel: Move to threads.

--- mqueue.h/t_mq_parallel.c
+++ mqueue.h/t_mq_parallel.c
@@ -1,8 +1,9 @@
 #include <assert.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <mqueue.h>
-#include <signal.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -10,129 +11,123 @@
 #include <sys/wait.h>
 
 #define LOGFILE		"logfile"
-#define	MQNAME		"/tmqpcmp1"
-#define NMESSAGES	10000
-#define MAXPRIO		32
+#define	MQNAME		"/abcde"
+#define NMESSAGES	20000
+#define MAXPRIO		100
 
 mqd_t md;
+int fd;
+
+pthread_mutex_t mtx_aq;
 
 /* Function prototypes. */
-static void myhandler(int sig);
+static void *thread1(void *arg);
+static void *thread2(void *arg);
 
 int main(void)
 {
-	char buf[100];
-	size_t i;
-	int fd, rv;
-	pid_t pid;
+	pthread_t th1;
+	pthread_t th2;
+	mqd_t md;
 
-	/*
-	 * Create log file.
-	 * The file must be created in O_WRONLY or O_RDWR mode,
-	 * or else lockf(3) will be very very unhappy.
-	 */
-	fd = open(LOGFILE, O_CREAT | O_TRUNC | O_WRONLY, 0777);
-	assert(fd != -1);
+	/* Initialize mutex. */
+	assert(pthread_mutex_init(&mtx_aq, NULL) == 0);
 
-	/* Install signal handler. */
-	signal(SIGABRT, myhandler);
+	/* Create log file. */
+        fd = open(LOGFILE, O_CREAT | O_TRUNC | O_WRONLY, 0777);
+        assert(fd != -1);
 
-	/* Create a message queue for write only with default parameters. */
-	md = mq_open(MQNAME, O_CREAT | O_EXCL | O_RDWR, 0700, NULL);
-	assert (md != -1);
+	/* Create a message queue for read-write with default parameters. */
+        md = mq_open(MQNAME, O_CREAT | O_EXCL | O_NONBLOCK | O_RDWR,
+	    0700, NULL);
+        assert (md != -1);
 
-	/* Fork and let the game begin. */
-	pid = fork();
-	assert(pid != -1);
+	/* Create threads. */
+	assert(pthread_create(&th1, NULL, thread1, (void *)&md) == 0);
+	assert(pthread_create(&th2, NULL, thread2, (void *)&md) == 0);
 
-	if (pid != 0) {
-		/* We are inside the parent. */
-	  printf("parent = %d\n", getpid());
+	/* Wait for threads to complete. */
+	assert(pthread_join(th1, NULL) == 0);
+	assert(pthread_join(th2, NULL) == 0);
 
+	/* Destroy mutex. */
+	assert(pthread_mutex_destroy(&mtx_aq) == 0);
 
-		for (i = 0; i < NMESSAGES; i++) {
+	/* Close log file. */
+	assert(close(fd) != -1);
 
-			snprintf(buf, sizeof(buf), "%d", i % MAXPRIO);
+	assert(mq_close(md) != -1);
+	assert(mq_unlink(MQNAME) != -1);
 
-                        /* Send messages with increasing priority. */
-                        rv = mq_send(md, buf, sizeof(buf),
-                            /* priority */ i % MAXPRIO);
-                        assert(rv != -1);
+	printf("passed\n");
 
-			snprintf(buf, sizeof(buf), "p: %d\n", i % MAXPRIO);
-			//assert(lockf(fd, F_LOCK, 0) != -1);
-			if (lockf(fd, F_LOCK, (off_t)0) == -1) {
-				perror("lockf parent: ");
-				assert(0);
-			}
-			write(fd, buf, strlen(buf));
-			assert(lockf(fd, F_ULOCK, (off_t)0) != -1);
-		}
-
-		assert(close(fd) != -1);
-
-		/* Disassociate with message queue. */
-		rv = mq_close(md);
-		assert(rv != -1);
-
-		/* Wait until child has read all messages. */
-		int status;
-		wait(&status);
-
-                /* Remove the message queue from the system. */
-                /*rv = mq_unlink(MQNAME);
-		perror("mq_unlink");
-                assert(rv != -1);*/
-
-                printf("passed\n");
-	} else {
-	  printf("child = %d\n", getpid());
-		/* We are inside the child. */
-		char msg_recvd[8192];   /* Implementation defined. */
-		unsigned int oldprio = UINT_MAX, prio, priostr;
-
-		for (i = 0; i < NMESSAGES; i++) {
-			rv = mq_receive(md, msg_recvd, sizeof(msg_recvd),
-			    &prio);
-			assert(rv != -1);
-
-			snprintf(buf, sizeof(buf), "c: %s\n", msg_recvd);
-			assert(lockf(fd, F_LOCK, (off_t)0) != -1);
-			write(fd, buf, strlen(buf));
-			assert(lockf(fd, F_ULOCK, (off_t)0) != -1);
-
-			//printf("msg = %s\n", msg_recvd);
-			//fflush(NULL);
-
-			priostr = atoi(msg_recvd);
-			//printf("%d\n", priostr);
-			assert(priostr == prio);
-
-			//printf("prio = %d\toldprio = %d\n", prio, oldprio);
-			//assert(prio < oldprio);
-			oldprio = prio;
-
-			//printf("prio = %d\tpriostr = %d\toldprio = %d\n",
-			//prio, priostr, oldprio);
-		}
-
-	}
-
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
 
-static void
-myhandler(int sig)
+static void *
+thread1(void *arg)
 {
-	/*
-	 * Message queues' name & resources are persistent, i.e., they live
-	 * even after the process dies. That's why, disassociate and destroy
-	 * the queue on failure, or else we might end up with zombie queues and
-	 * hit the limit of max open queues.
-	 * Also, we don't care about the return value of the following calls.
-	 */
-	mq_close(md);
-	mq_unlink(MQNAME);
+	char buf[100];
+	mqd_t md;
+	int i, rv;
 
-	/* After this, the program will abort. */
+	/* Retrieve argument. */
+	md = *(mqd_t *)arg;
+
+	/* Send messages with increasing priority. */
+	for (i = 0; i < NMESSAGES; i++) {
+		snprintf(buf, sizeof(buf), "parent: %d\n", i % MAXPRIO);
+
+		pthread_mutex_lock(&mtx_aq);
+
+		rv = mq_send(md, buf, sizeof(buf),
+		    /* priority */ i % MAXPRIO);
+		if (rv == -1 && errno == EAGAIN) {
+			pthread_mutex_unlock(&mtx_aq);
+			usleep(10);
+			i--;
+			continue;
+		}
+		assert(rv != -1);
+
+		write(fd, buf, strlen(buf));
+
+		pthread_mutex_unlock(&mtx_aq);
+	}
+
+	pthread_exit(NULL);
+}
+
+static void *
+thread2(void *arg)
+{
+	char msg_recvd[8192];   /* Implementation defined. */
+	char buf[100];
+	unsigned int prio;
+        mqd_t md;
+	int i, rv;
+
+        /* Retrieve argument. */
+        md = *(mqd_t *)arg;
+
+	for (i = 0; i < NMESSAGES; i++) {
+		pthread_mutex_lock(&mtx_aq);
+
+		rv = mq_receive(md, msg_recvd, sizeof(msg_recvd),
+		    &prio);
+		if (rv == -1 && errno == EAGAIN) {
+			pthread_mutex_unlock(&mtx_aq);
+			usleep(10);
+			i--;
+			continue;
+		}
+		assert(rv != -1);
+
+		snprintf(buf, sizeof(buf), "child: %d\n", prio);
+		write(fd, buf, strlen(buf));
+
+		pthread_mutex_unlock(&mtx_aq);
+	}
+
+	pthread_exit(NULL);
 }
