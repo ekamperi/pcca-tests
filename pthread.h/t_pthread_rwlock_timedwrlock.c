8743bc94d668bdb230d203d7aa2a3b633fe192ed
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-27 23:30:46 +0300

    t_pthread_rwlock_timedwrlock: Don't call _timedrdlock() from main thread.

--- pthread.h/t_pthread_rwlock_timedwrlock.c
+++ pthread.h/t_pthread_rwlock_timedwrlock.c
@@ -6,11 +6,14 @@
 #include <time.h>
 #include <sys/time.h>
 
+pthread_rwlock_t rwl;
+
+/* Function prototypes. */
+static void *thread(void *);
+
 int main(void)
 {
-	pthread_rwlock_t rwl;
-	struct timespec abstime;
-	struct timeval now;
+	pthread_t tid;
 
 	/* Create read-write lock with default parameters. */
 	assert(pthread_rwlock_init(&rwl, NULL) == 0);
@@ -19,6 +22,31 @@ int main(void)
 	assert(pthread_rwlock_rdlock(&rwl) == 0);
 	assert(pthread_rwlock_rdlock(&rwl) == 0);
 	assert(pthread_rwlock_rdlock(&rwl) == 0);
+
+	/* Create thread. */
+	assert(pthread_create(&tid, NULL, thread, (void *)NULL) == 0);
+
+	/* Wait for thread to complete. */
+	assert(pthread_join(tid, NULL) == 0);
+
+	/* Release read lock. */
+	assert(pthread_rwlock_unlock(&rwl) == 0);
+	assert(pthread_rwlock_unlock(&rwl) == 0);
+	assert(pthread_rwlock_unlock(&rwl) == 0);
+
+	/* Destroy read-write lock. */
+	assert(pthread_rwlock_destroy(&rwl) == 0);
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
+
+static void *
+thread(void *arg)
+{
+        struct timespec abstime;
+        struct timeval now;
 
 	/* Try to acquire a write lock. */
 	abstime.tv_sec = 0;
@@ -30,14 +58,5 @@ int main(void)
 	abstime.tv_nsec = 1000 * now.tv_usec;
 	assert(pthread_rwlock_timedwrlock(&rwl, &abstime) == ETIMEDOUT);
 
-	assert(pthread_rwlock_unlock(&rwl) == 0);
-	assert(pthread_rwlock_unlock(&rwl) == 0);
-	assert(pthread_rwlock_unlock(&rwl) == 0);
-
-	/* Destroy read-write lock. */
-	assert(pthread_rwlock_destroy(&rwl) == 0);
-
-	printf("passed\n");
-
-	return (EXIT_SUCCESS);
+	pthread_exit(NULL);
 }
