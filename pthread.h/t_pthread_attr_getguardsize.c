d3a9d4b90c7bfa18e8287bc6ebddc1ff7f259a10
Stathis Kamperis <beket@dragonflybsd.org>
2009-06-28 14:35:04 +0300

    t_pthread_attr_getguardsize: Test _setguardsize() as well.

--- pthread.h/t_pthread_attr_getguardsize.c
+++ pthread.h/t_pthread_attr_getguardsize.c
@@ -12,8 +12,21 @@ int main(void)
 	/* Initialize thread attribute object. */
 	assert(pthread_attr_init(&attr) == 0);
 
+	/* Invalid guard size. */
+	assert(pthread_attr_getguardsize(&attr, NULL) == EINVAL);
+
 	/* Get size of guard page. */
 	assert(pthread_attr_getguardsize(&attr, &guardsize) == 0);
+
+	/* Set size of guard page. */
+	guardsize = 8000;
+	assert(pthread_attr_setguardsize(&attr, &guardsize) == 0);
+	assert(pthread_attr_getguardsize(&attr, &guardsize) == 0);
+	/*
+	 * We take into consideration the fact that the implementation
+	 * may round guard size up to a multiple of page size.
+	 */
+	assert(guardsize >= 8000);
 
 	/* Destroy thread attribute object. */
 	assert(pthread_attr_destroy(&attr) == 0);
