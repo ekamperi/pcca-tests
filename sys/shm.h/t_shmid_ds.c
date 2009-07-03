0d4cc8ed1730c64ca24a67b2f87363cb833b735c
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-03 15:17:04 +0300

    Add test case for shmid_ds data structure.

---
+++ sys/shm.h/t_shmid_ds.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+
+int main(void)
+{
+	struct shmid_ds s;
+
+	/* POSIX mandates the following members. */
+	(void)s.shm_perm;	/* Operation permission structure. */
+	(void)s.shm_segsz;	/* Size of segment in bytes. */
+	(void)s.shm_lpid;	/* ProcessID of last shared memory operation. */
+	(void)s.shm_cpid;	/* ProcessID of creator. */
+	(void)s.shm_nattch;	/* Number of current attaches. */
+	(void)s.shm_atime;	/* Time of last shmat(). */
+	(void)s.shm_dtime;	/* Time of last shmdt(). */
+	(void)s.shm_ctime;	/* Time of last change by shmctl(). */
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
