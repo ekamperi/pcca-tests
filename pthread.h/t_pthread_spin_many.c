cbf2b9b9c663b82ea80b510fc2d6f4952c12b665
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-05 00:18:27 +0300

    Draft version of t_pthread_spin_many.

---
+++ pthread.h/t_pthread_spin_many.c
@@ -0,0 +1,69 @@
+#include <assert.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>	/* for sleep(3) */
+
+#define NTHREADS	10
+
+pthread_spinlock_t lock;
+int cnt = 0;
+
+/* Function prototypes. */
+static void *thread(void *);
+
+int main(void)
+{
+	pthread_t thpool[NTHREADS];
+	size_t i;
+
+	/* Initialize spinlock. */
+	assert(pthread_spin_init(&lock, PTHREAD_PROCESS_PRIVATE) == 0);
+
+        /* Acquire spinlock. */
+        assert(pthread_spin_lock(&lock) == 0);
+
+	/* Create threads. */
+	for (i = 0; i < NTHREADS; i++)
+		assert(pthread_create(&thpool[i], NULL, thread, (void *)NULL)
+		    == 0);
+
+	/* Sleep so that the threads spin a bit. */
+	sleep(1);
+
+	/* Enough is enough -- release spinlock. */
+	assert(pthread_spin_unlock(&lock) == 0);
+
+	/*
+	 * Wait for threads to complete.
+	 * Each thread acquires the spinlock, so the rest will
+	 * spin again a bit.
+	 */
+	for (i = 0; i < NTHREADS; i++)
+		assert(pthread_join(thpool[i], NULL) == 0);
+
+	/* Destroy spinlock. */
+	assert(pthread_spin_destroy(&lock) == 0);
+
+	/* Make sure that NTHREADS were ran. */
+	assert(cnt == NTHREADS);
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
+
+static void *
+thread(void *arg)
+{
+	/* Acquire spinlock. */
+	assert(pthread_spin_lock(&lock) == 0);
+
+	cnt++;
+
+	/* ... and immediately release it. */
+	assert(pthread_spin_unlock(&lock) == 0);
+
+	pthread_exit(NULL);
+}
