141a4d7d97fa79dc01639a21242f24640e7da387
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-09 19:18:15 +0300

    Add t_dlsym test case.

---
+++ dlfcn.h/t_dlsym.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+int main(void)
+{
+	void *handle;
+	int (*pstrcmp)(const char *s1, const char *s2);
+
+	/* Open shared object. */
+	handle = dlopen("libc.so", RTLD_LAZY);
+	assert(handle != NULL);
+
+	/* Get the address of strcmp(3). */
+	*(void **)(&pstrcmp) = dlsym(handle, "strcmp");
+	assert(dlerror() == NULL);
+
+	/* Invoke strcmp(). */
+	assert((*pstrcmp)("abc", "abc") == 0);
+	assert((*pstrcmp)("a", "b") < 0);
+	assert((*pstrcmp)("b", "a") > 0);
+
+	/* Close shared object. */
+	assert(dlclose(handle) == 0);
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
