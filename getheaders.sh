bdf7cc183feebea6aba9677649d16cda266744e6
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-10 21:44:58 +0300

    getheaders.sh: Redirect stderr to null (for sandbox related perms errors).

--- getheaders.sh
+++ getheaders.sh
@@ -3,4 +3,4 @@
 # Exit immediately if any untested command fails in non-interactive mode.
 set -e
 
-find . -type d -name "*.h" | sed 's/.\///' | sort
+find . -type d -name "*.h" 2>/dev/null | sed 's/.\///' | sort
