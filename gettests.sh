5b7e50aed81854bbd185a095d109036f8c270ce1
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-11 12:13:58 +0300

    gettests.sh: Print usage if no argument is given.

--- gettests.sh
+++ gettests.sh
@@ -3,4 +3,11 @@
 # Exit immediately if any untested command fails in non-interactive mode.
 set -e
 
+# If no argument is given, print usage.
+if [ -z "$1" ]
+then
+    echo "Usage: $0 <header>"
+    echo "To get a list of available headers, please run gettheaders.sh"
+fi
+
 find . -name "$1" -exec cat {}/tfile \; 2>/dev/null
