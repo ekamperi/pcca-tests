b834de6946603805642dcc96c42a87f654d04350
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-11 22:04:47 +0300

    t_fchdir: Add more tests plus convert to sandbox/.

--- unistd.h/t_fchdir.c
+++ unistd.h/t_fchdir.c
@@ -1,17 +1,28 @@
 #include <assert.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main(void)
 {
+	int fd;
+
+	/* Search permission is denied for the directory referenced by fd. */
+	fd = open("sandbox/dir666", O_RDONLY);
+	assert(fd != -1);
+	assert(fchdir(fd) == -1 && errno == EACCES);
+	assert(close(fd) != -1);
 
 	/* Invalid (not open) file descriptor. */
 	assert(fchdir(-1) == -1 && errno == EBADF);
 
 	/* Open file descriptor, but not pointing to a directory. */
-	assert(fchdir(STDOUT_FILENO) == -1 && errno == ENOTDIR);
+	fd = open("sandbox/file777", O_RDONLY);
+	assert(fd != -1);
+	assert(fchdir(fd) == -1 && errno == ENOTDIR);
+	assert(close(fd) != -1);
 
 	printf("passed\n");
 
