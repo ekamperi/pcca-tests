f42b7e45f8860c150207b7b45e83271f5fa616c2
Stathis Kamperis <ekamperi@gmail.com>
2009-07-13 05:55:03 +0300

    Add test case for getlogin_r.

---
+++ unistd.h/t_getlogin_r.c
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+	long guessed, sz, rv;
+	char *p;
+
+	/* Get max login name. */
+	sz = -1;
+	guessed = 0;
+#ifdef	_SC_LOGIN_NAME_MAX
+	sz = sysconf(_SC_LOGIN_NAME_MAX);
+	assert(sz != -1);	/* It can't be infinite. */
+#endif
+
+	/* If querying sysconf database didn't work, just guess. */
+	if (sz == -1) {
+		sz = 1024;
+		guessed = 1;
+	}
+
+	/* Allocate memory for buffer (+1 for the \0). */
+	p = malloc(sz + 1);
+	assert(p != NULL);
+
+	/* Get login name, but if we guessed account for a potential error. */
+	rv = getlogin_r(p, sizeof(p));
+	assert(rv == 0
+	    || errno == ENXIO
+	    || (guessed == 1 && errno == ERANGE));
+
+	printf("passed");
+
+	/* Test against ERANGE error (optional). */
+	if (getlogin_r(p, 1) != ERANGE)
+		printf(" (ERANGE test skipped)");
+
+	printf("\n");
+
+	/* Free memory. */
+	free(p);
+
+	return (EXIT_SUCCESS);
+}
