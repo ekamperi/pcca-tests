9e3f029e0bb7c442c55767b5c5ff4254e01713f9
Stathis Kamperis <ekamperi@gmail.com>
2009-07-14 02:05:31 +0300

    Add half complete test case for link.

---
+++ unistd.h/t_link.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+	int rv;
+
+	/* A component of path prefix denies search permission. */
+	rv = link("sandbox/dir666/whatever", "sandbox/whatever");
+	assert(rv == -1 && errno == EACCES);
+
+	/*
+	 * The requested link requires writing in a directory that denies
+	 * write permission.
+	 */
+	rv = link("sandbox/file777", "sandbox/dir555/whatever");
+	assert(rv == -1 && errno == EACCES);
+
+	/* The `path2' argument resolves to an existing file. */
+	rv = link("sandbox/file777", "sandbox/file777");
+	assert(rv == -1 && errno == EEXIST);
+
+	/* The `path2' argument' refers to a symbolic link. */
+	rv = link("sandbox/file777", "sandbox/loop");
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
