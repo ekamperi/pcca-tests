772313f79c2a36bf379e067266179a203cf563cd
Stathis Kamperis <ekamperi@gmail.com>
2009-07-18 09:09:14 +0300

    t_creal: Increase input range.

--- complex.h/t_creal.c
+++ complex.h/t_creal.c
@@ -6,9 +6,15 @@
 
 int main(void)
 {
-	double complex z = 3.513 + 4.596 * I;
+        double rl, im;
+        double complex z;
 
-	assert(fabs(creal(z) - 3.513) < 0.001);
+        for (rl = -10.0; rl < 10.0; rl += 0.04) {
+		for (im = -10.0; im < 10.0; im += 0.03) {
+			z = rl + im*I;
+			assert(fabs(creal(z) - rl) < 0.00001);
+		}
+        }
 
 	printf("passed\n");
 
