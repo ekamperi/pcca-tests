d7ded6b142ce9cc1259ccdccf2a65f7233466a1d
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-22 11:26:29 +0300

    Add test case for ctanh.

---
+++ complex.h/t_ctanh.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2009, Stathis Kamperis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <complex.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tentry {
+	double complex x;	/* Input */
+	double complex y;	/* Hyperbolic tangent output */
+} ttable[] = {
+	{ 1.123 + 2.456*I,  0.938886 - 0.196969*I },
+	{-0.505 + 2.021*I, -1.275350 - 0.839293*I },
+	{ 0.587 - 0.667*I,  0.729036 - 0.48447*I },
+	{-0.101 - 1.052*I, -0.397083 - 1.681418*I }
+};
+
+int main(void)
+{
+	size_t i;
+
+	for (i = 0; i < sizeof(ttable) / sizeof(struct tentry); i++) {
+		/* Compare real parts. */
+		assert(fabs(creal(ctanh(ttable[i].x)) - creal(ttable[i].y))
+		    < 0.001);
+
+		/* Compare imaginary parts. */
+		assert(fabs(cimag(ctanh(ttable[i].x)) - cimag(ttable[i].y))
+		    < 0.001);
+	}
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
