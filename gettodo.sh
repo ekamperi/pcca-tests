20dce0da116fdf842c22d28a8b73bfb782b3cb6e
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-23 09:21:58 +0300

    Add gettodo.sh.

---
+++ gettodo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Exit immediately if any untested command fails in non-interactive mode.
+set -e
+
+for t in $(find . -type f -name TODO 2>/dev/null); do
+    echo ">>> $(echo $t | sed 's/.\///')"
+    cat "$t"
+    echo
+done
