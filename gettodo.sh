12f1376633aa5674b15c6f5cd8ffaca35a944990
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-24 15:44:34 +0300

    gettodo.sh: Sort paths.

--- gettodo.sh
+++ gettodo.sh
@@ -3,7 +3,7 @@
 # Exit immediately if any untested command fails in non-interactive mode.
 set -e
 
-for t in $(find . -type f -name TODO 2>/dev/null); do
+for t in $(find . -type f -name TODO 2>/dev/null | sort); do
     echo ">>> $(echo $t | sed 's/.\///')"
     cat "$t"
     echo
