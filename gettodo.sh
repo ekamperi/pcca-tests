8ab8fc843631877af2c62c20809a873f0979876e
Stathis Kamperis <beket@dragonflybsd.org>
2009-07-24 16:14:30 +0300

    gettodo.sh: Skip .git/.

--- gettodo.sh
+++ gettodo.sh
@@ -3,7 +3,9 @@
 # Exit immediately if any untested command fails in non-interactive mode.
 set -e
 
-for t in $(find . -type f -name TODO 2>/dev/null | sort); do
+for t in $(find . -name .git -prune -o -type f -name TODO -print 2>/dev/null \
+    | sort)
+do
     echo ">>> $(echo $t | sed 's/.\///')"
     cat "$t"
     echo
