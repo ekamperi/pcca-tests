27fd9cd369956b5e98d627fd1c17f0434a5c947f
Stathis Kamperis <beket@dragonflybsd.org>
2009-08-04 20:51:04 +0300

    t_uname: Add more tests.

--- sys/utsname.h/t_uname.c
+++ sys/utsname.h/t_uname.c
@@ -5,7 +5,22 @@
 
 int main(void)
 {
+	/*
+	 * The `utsname' structure must be defined, including at least
+	 * the following members.
+	 */
 	struct utsname un;
+
+	(void)un.sysname;	/* Name of this implementation of the operating
+				   system. */
+	(void)un.nodename;	/* Name of this node within the communications
+				   network to which this node is attached,
+				   if any. */
+	(void)un.release;	/* Current release level of this
+				   implementation. */
+	(void)un.version;	/* Current version level of this release. */
+	(void)un.machine;	/* Name of the hardware type on which the system
+				   is running. */
 
 	/* We expect this simple call to succeed.
 	 * POSIX doesn't define any required errnos, but various implementations
