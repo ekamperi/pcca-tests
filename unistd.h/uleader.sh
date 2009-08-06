8c7fc224e76a14fbe1f00253d01dc5ceb646b722
Stathis Kamperis <beket@dragonflybsd.org>
2009-08-06 22:12:18 +0300

    uleader.sh: Add comments.

--- unistd.h/uleader.sh
+++ unistd.h/uleader.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
+#
+# I have tried to read the source of this script a month after I wrote it
+# and found it difficult to understand what was doing. Here is the deal:
+#
+# We have a template file in the current directory, called `template.c'.
+# This is NOT compilable by default. We use it as a skeleton file, where
+# we replace some of its contents to turn it into compilable. We then
+# build it and run it as any other regular test case in our src tree.
+#
+# So what exactly do we replace?
+#
+# We replace _POSIX_MYSYMBOL and _SC_MYMBOL with the real symbols, which
+# are stored in the `ffile'. Actually we store only the _POSIX symbols
+# and we use sed(1) to generate the _SC equivalents.
 
 while read posix_symbol
 do
-  # In `ffile' we store the symbolic constants as defined in <unistd.h>.
   # We replace the _POSIX part with _SC to get the name of the argument
-  # that we will pass to sysconf(3).
+  # that we will be passing to sysconf(3).
   sc_symbol=$(echo $posix_symbol | sed 's/_POSIX/_SC/')
 
   # Construct C source file name (we use lowercase).
