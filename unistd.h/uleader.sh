4659ac844699133b028113f919b279527c79f922
Stathis Kamperis <beket@dragonflybsd.org>
2009-08-07 07:28:28 +0300

    uleader.sh: Fix indentation, plus add a comment.

--- unistd.h/uleader.sh
+++ unistd.h/uleader.sh
@@ -16,23 +16,23 @@
 
 while read posix_symbol
 do
-  # We replace the _POSIX part with _SC to get the name of the argument
-  # that we will be passing to sysconf(3).
-  sc_symbol=$(echo $posix_symbol | sed 's/_POSIX/_SC/')
+    # We replace the _POSIX part with _SC to get the name of the argument
+    # that we will be passing to sysconf(3).
+    sc_symbol=$(echo $posix_symbol | sed 's/_POSIX/_SC/')
 
-  # Construct C source file name (we use lowercase).
-  c_file=$(echo "$posix_symbol.c" | tr '[:upper:]' '[:lower:]')
+    # Construct C source file name (we use lowercase).
+    c_file=$(echo "$posix_symbol.c" | tr '[:upper:]' '[:lower:]')
 
-  # Inject the right symbols into the C source file.
-  sed "s/_POSIX_MYSYMBOL/$posix_symbol/g" template.c > "tmp$c_file"
-  sed "s/_SC_MYSYMBOL/$sc_symbol/g" "tmp$c_file" > "$c_file"
-  rm -f "tmp$c_file"
+    # Inject the right symbols into the C source file.
+    sed "s/_POSIX_MYSYMBOL/$posix_symbol/g" template.c > "tmp$c_file"
+    sed "s/_SC_MYSYMBOL/$sc_symbol/g" "tmp$c_file" > "$c_file"
+    rm -f "tmp$c_file"
 
-  # Compile
-  gcc "$c_file" #>/dev/null 2>/dev/null
-  rm "$c_file"
+    # Compile, run, report, cleanup.
+    gcc "$c_file" #>/dev/null 2>/dev/null
+    rm "$c_file"
 
-  printf "$(echo "/unistd.h/t$c_file: " | sed 's/.c//g')"
-  ./a.out
-  rm a.out
+    printf "$(echo "/unistd.h/t$c_file: " | sed 's/.c//g')"
+    ./a.out
+    rm a.out
 done < ffile
