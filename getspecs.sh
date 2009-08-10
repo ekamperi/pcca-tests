19cf58ee84282d265581cf229e184506b3512314
Stathis Kamperis <beket@dragonflybsd.org>
2009-08-11 00:49:55 +0300

    getspecs.sh: Skip .git/.

--- getspecs.sh
+++ getspecs.sh
@@ -3,4 +3,5 @@
 # Exit immediately if any untested command fails in non-interactive mode.
 set -e
 
-find . -type f -name "*.h.spec" -exec basename {} \; 2>/dev/null | sort
+find . -name .git -prune -o \( -type f -name "*.h.spec" -exec basename {} \; \) \
+    2>/dev/null | sort
