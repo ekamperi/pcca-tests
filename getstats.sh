475ed1cf62c9c6926073634f6abd52f7643e067c
Stathis Kamperis <beket@dragonflybsd.org>
2009-08-10 16:05:59 +0300

    getstats.sh: Major cleanup. Return correct stats from find(1).

--- getstats.sh
+++ getstats.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
-NHEADERS=$(find . -name .git -prune -o -type d -name "*.h" 2>/dev/null | wc -l)
-NTESTS=$(find . -name .git -prune -o -type f -name "tfile" 2>/dev/null | xargs cat | wc -l)
+# Number of audited headers
+NHEADERS=$(find . -name .git -prune -o \( -type d -name "*.h" -print \) \
+    2>/dev/null | wc -l)
 
-CLOC=$(find . -name .git -prune -o -name "*.c" 2>/dev/null | xargs cat | wc -l)
-MAKELOC=$(find . -name .git -prune -o -name "Makefile" 2>/dev/null | xargs cat | wc -l)
-SHLOC=$(find . -name .git -prune -o -name "*.sh" 2>/dev/null | xargs cat | wc -l)
-PERLLOC=$(find . -name .git -prune -o -name "*.pl" 2>/dev/null | xargs cat | wc -l)
+# Number of test cases (as found in tfiles)
+NTESTS=$(find . -name .git -prune -o \( -type f -name "tfile" -print \) \
+    2>/dev/null | xargs cat | wc -l)
+
+# Number of lines of code in .c files
+CLOC=$(find . -name .git -prune -o \( -name "*.c" -print \) 2>/dev/null \
+    | xargs cat | wc -l)
+
+# Number of lines of code in Makefiles
+MAKELOC=$(find . -name .git -prune -o \( -name "Makefile" -print \) \
+    2>/dev/null | xargs cat | wc -l)
+
+# Number of lines of code in shell scripts
+SHLOC=$(find . -name .git -prune -o \( -name "*.sh" -print \) 2>/dev/null \
+    | xargs cat | wc -l)
+
+# Number of lines of code in Perl scripts
+PERLLOC=$(find . -name .git -prune -o \( -name "*.pl" -print \) 2>/dev/null \
+    | xargs cat | wc -l)
 
 echo ">>> Headers"
 printf "\tAudited: $NHEADERS\n"
@@ -22,8 +38,13 @@ printf "\tmake   : $MAKELOC\n"
 printf "\tsh     : $SHLOC\n"
 printf "\tPerl   : $PERLLOC\n"
 
-NPASSED=$(find . -name "log.passed" 2>/dev/null | xargs cat | wc -l)
-NFAILED=$(find . -name "log.failed" 2>/dev/null | xargs cat | wc -l)
+# Number of tests that passed
+NPASSED=$(find . -name .git -prune -o \( -name "log.passed" -print \) \
+    2>/dev/null | xargs cat | wc -l)
+
+# Number of tests that failed
+NFAILED=$(find . -name .git -prune -o \( -name "log.failed" -print \) \
+    2>/dev/null | xargs cat | wc -l)
 
 echo
 echo ">>> Results from last run"
