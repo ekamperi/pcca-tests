322a3d9c870e0f51ee3276970de7ba991c84a8b4
Stathis Kamperis <beket@dragonflybsd.org>
2009-08-11 02:22:52 +0300

    mqueue.h: Add licenses.

--- mqueue.h/t_mq_setattr.c
+++ mqueue.h/t_mq_setattr.c
@@ -1,12 +1,39 @@
+/*
+ * Copyright (c) 2009, Stathis Kamperis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <mqueue.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>	/* memset */
+#include <string.h>	/* for memset() */
 
-#define	MQNAME	"/tmqsa"
+#define	MQNAME	"/t_mq_setattr"
 
 int main(void)
 {
@@ -28,8 +55,8 @@ int main(void)
 	assert(oldattr.mq_msgsize == attr.mq_msgsize);
 	assert(oldattr.mq_curmsgs == 0 && attr.mq_curmsgs == 0);
 
-	mq_close(md);
-	mq_unlink(MQNAME);
+	assert(mq_close(md) != -1);
+	assert(mq_unlink(MQNAME) != -1);
 
 	printf("passed\n");
 
