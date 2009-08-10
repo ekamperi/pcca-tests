a2aad1bc77ff1ae2bd0cf1ce0e69c00b42f7f3e8
Stathis Kamperis <beket@dragonflybsd.org>
2009-08-10 13:47:17 +0300

    getstats.sh: Add pass/fail stats.

--- getstats.sh
+++ getstats.sh
@@ -8,12 +8,24 @@ MAKELOC=$(find . -name "Makefile" 2>/dev/null | xargs cat | wc -l)
 SHLOC=$(find . -name "*.sh" 2>/dev/null | xargs cat | wc -l)
 PERLLOC=$(find . -name "*.pl" 2>/dev/null | xargs cat | wc -l)
 
-echo "Audited headers: $NHEADERS"
-echo "Tests (tfiles) : $NTESTS"
-echo
+echo ">>> Headers"
+printf "\tAudited: $NHEADERS\n"
 
-echo "Lines of code:"
-printf "\tC   : $CLOC\n"
-printf "\tmake: $MAKELOC\n"
-printf "\tsh  : $SHLOC\n"
-printf "\tPerl: $PERLLOC\n"
+echo
+echo ">>> Test files"
+printf "\ttfiles : $NTESTS\n"
+
+echo
+echo ">>> Lines of code"
+printf "\tC      : $CLOC\n"
+printf "\tmake   : $MAKELOC\n"
+printf "\tsh     : $SHLOC\n"
+printf "\tPerl   : $PERLLOC\n"
+
+NPASSED=$(find . -name "log.passed" 2>/dev/null | xargs cat | wc -l)
+NFAILED=$(find . -name "log.failed" 2>/dev/null | xargs cat | wc -l)
+
+echo
+echo ">>> Results from last run"
+printf "\tPassed : $NPASSED\n"
+printf "\tFailed : $NFAILED\n"
