4acfdc4fefa62c761e7bf765267fbb8892459097
stathis <stathis@opensolaris.(none)>
2009-08-31 23:30:55 +0300

    Initial import of popdbfunc.sh.

---
+++ popdbfunc.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# If no argument is given, print usage.
+if [ -z "$1" ]; then
+    echo "Usage: $0 /path/to/header/functions.list"
+    exit 1
+fi
+
+# First line is header.
+{
+    read header
+
+    while read func; do
+	# Skip empty lines.
+	if [ -z "$func" ]; then
+	    continue
+	fi
+
+	manpage="yes"
+	if ! man "$func" >/dev/null 2>/dev/null; then
+	    manpage="no"
+	fi
+
+        printf "INSERT INTO psx_functions VALUES('$header', '$func', 'pending',"
+	printf " 'pending', 'pending', 'pending', '$manpage');\n"
+    done
+} < "$1"
