ffd282bcc2f0ef1a35700dbc82bdb8bf87b4ca64
stathis <stathis@opensolaris.(none)>
2009-09-01 20:48:33 +0300

    Initial import of popdbfunc.sh

---
+++ dbpop/popdbfunc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Exit immediately if any untested command fails in non-interactive mode.
+set -e
+
+# Unless explicitly provided, use .. directory as the default one.
+if [ ! -z "$1" ]; then
+    STARTDIR="$1"
+else
+    STARTDIR=".."
+fi
+
+find "$STARTDIR" -name .git -prune -o \( -type d -name "*.h" -exec \
+    ./popheaderfunc.sh {}/functions.list \; \) 2>/dev/null | sort
