0256e9f80059aa7dcf6b1ee260bb8424be3126e9
stathis <stathis@opensolaris.(none)>
2009-09-21 16:35:03 +0300

    t_tgamma: Add more tests.

--- math.h/t_tgamma.c
+++ math.h/t_tgamma.c
@@ -4,11 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct tentry {
+	double x;       /* Input */
+	double y;       /* Gamma output */
+} ttable[] = {
+	{ 1.000,    1.000 },
+	{ 1.111,    0.947 },
+	{ 3.000,    2.000 },
+	{ 3.309,    2.708 },
+	{ 5.000,   24.000 },
+	{ 5.050,   25.881 },
+	{ 7.000,  720.000 },
+	{ 7.777, 3226.060 }
+};
+
 int main(void)
 {
-	double rv;
+        size_t i;
+
+        for (i = 0; i < sizeof(ttable) / sizeof(struct tentry); i++)
+                assert(fabs(tgamma(ttable[i].x) - ttable[i].y) < 0.01);
 
 	/* If x is NaN, a NaN shall be returned. */
+	double rv;
+
 	rv = tgamma(nan(""));
 	assert(isnan(rv) != 0);
 
