a82daa0a64b445189e86e17cbcc17a53273c9915
stathis <stathis@opensolaris.(none)>
2009-09-23 14:49:43 +0300

    t_rint: Remove tons of whitespace.

--- math.h/t_rint.c
+++ math.h/t_rint.c
@@ -30,12 +30,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*                                                                                                                     
- * The rint*() functions return the integral value (represented as a                                                   
- * double) nearest x in the direction of the current rounding mode.                                                    
- *                                                                                                                     
- * POSIX says that the rounding mode is implementation-defined, so                                                     
- * that leaves us in the dark, I guess.                                                                                
+/*
+ * The rint*() functions return the integral value (represented as a
+ * double) nearest x in the direction of the current rounding mode.
+ *
+ * POSIX says that the rounding mode is implementation-defined, so
+ * that leaves us in the dark, I guess.
  */
 
 struct tentry {
@@ -45,12 +45,12 @@ struct tentry {
 	{   1.010,     1.000 },
 	{  -1.010,    -1.000 },
 	{   2.411,     2.000 },
-	{  -2.411,    -2.000 }, 
+	{  -2.411,    -2.000 },
 	{   3.999,     4.000 },
 	{  -3.999,    -4.000 },
 	{   9.997,    10.000 },
 	{  -9.997,   -10.000 },
-	{  101.005,  101.000 }, 
+	{  101.005,  101.000 },
 	{ -101.005, -101.000 },
 	{  507.777,  508.000 },
 	{ -507.777, -508.000 }
@@ -60,9 +60,9 @@ int main(void)
 {
 	size_t i;
 
-        for (i = 0; i < sizeof(ttable) / sizeof(struct tentry); i++)
-                assert(fabs(rint(ttable[i].x) - ttable[i].y) < 0.001);
-	
+	for (i = 0; i < sizeof(ttable) / sizeof(struct tentry); i++)
+		assert(fabs(rint(ttable[i].x) - ttable[i].y) < 0.001);
+
 	/* If x is NaN, a NaN shall be returned. */
 	assert(isnan(rint(nan(""))) != 0);
 
