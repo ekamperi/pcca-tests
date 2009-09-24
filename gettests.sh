1ddd42fd6ed7ad9e7a95a0275abc702cda531a34
stathis <stathis@opensolaris.(none)>
2009-09-24 17:06:35 +0300

    gettests.sh: Now you can specify the top level dir.

--- gettests.sh
+++ gettests.sh
@@ -6,10 +6,10 @@ set -e
 # If no argument is given, print usage.
 if [ -z "$1" ]
 then
-    echo "usage: $0 <header>"
-    echo "To get a list of available headers, please run getheaders.sh"
-    exit 1
+    STARTDIR="."
+else
+    STARTDIR="$1"
 fi
 
-find . -name .git -prune -o \( -name "$1" -type d -exec cat {}/tfile \; \) \
+find "$STARTDIR" -name .git -prune -o \( -name "*.h" -type d -exec cat {}/tfile \; \) \
     2>/dev/null | sort
