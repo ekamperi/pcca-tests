528e90df327f3340536d5967238310affbabd59e
stathis <stathis@opensolaris.(none)>
2009-10-04 16:24:49 +0300

    ftw.h/t_ftw: Cleanup a bit.

--- ftw.h/t_ftw.c
+++ ftw.h/t_ftw.c
@@ -35,17 +35,19 @@
 /* Function prototypes. */
 static int mycallback(const char *, const struct stat *, int);
 
-int main(int argc, char *argv[])
+int
+main(void)
 {
-	/* ndirs isn't in the range [1, {OPEN_MAX}] */
-	assert(ftw(".", mycallback, 0) == -1 && errno == EINVAL);
+	/* The ndirs argument isn't in the range [1, {OPEN_MAX}]. */
+	assert(ftw(".", mycallback, -1) == -1 && errno == EINVAL);
+	assert(ftw(".", mycallback,  0) == -1 && errno == EINVAL);
 #ifdef OPEN_MAX
 	assert(ftw(".", mycallback, OPEN_MAX + 1) == -1 && errno == EINVAL);
 #endif
 
-	/* Inexistent path. */
+	/* Empty or inexistent path. */
 	assert(ftw("", mycallback, 1) == -1 && errno == ENOENT);
-	assert(ftw("/sandbox/thisdefinitelydoesntexist", mycallback, 1) == -1 &&
+	assert(ftw("sandbox/thisdefinitelydoesntexist", mycallback, 1) == -1 &&
 	       errno == ENOENT);
 
 	printf("passed\n");
