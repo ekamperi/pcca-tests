8e6f5ce7a5289453193272cd9073fe205d21ce99
stathis <stathis@opensolaris.(none)>
2009-11-26 18:03:51 +0200

    Initial import of t_nearbyint.

---
+++ math.h/t_nearbyint.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2009, Stathis Kamperis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+	/* If x is NaN, a NaN shall be returned. */
+	assert(isnan(nearbyint(nan(""))) != 0);
+
+	/* If x is ±0, ±0 shall be returned. */
+	assert(signbit(nearbyint(+0)) == 0);
+	assert(signbit(nearbyint(-0)) != 0);
+
+	/* If x is ±Inf, x shall be returned. */
+#ifdef INFINITY
+	double x;
+
+	x = +INFINITY;
+	assert(isinf(nearbyint(x)) && signbit(x) == 0);
+
+	x = -INFINITY;
+	assert(isinf(nearbyint(x)) && signbit(x) != 0);
+#endif
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
