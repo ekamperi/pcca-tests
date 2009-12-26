5ff3b9b8a4f0e605020b2e4a3b6ef2d09fe88ff5
stathis <stathis@opensolaris.(none)>
2009-12-26 04:27:56 +0200

    Initial import of t_fenv.c

---
+++ fenv.h/t_fenv.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2009, Stathis Kamperis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <fenv.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+
+	/*
+	 * FE_ALL_EXCEPT is the bitwise-inclusive OR of all floating-point
+	 * exception constants.
+	 */
+	assert(FE_ALL_EXCEPT & FE_DIVBYZERO);
+	assert(FE_ALL_EXCEPT & FE_INEXACT);
+	assert(FE_ALL_EXCEPT & FE_INVALID);
+	assert(FE_ALL_EXCEPT & FE_OVERFLOW);
+	assert(FE_ALL_EXCEPT & FE_UNDERFLOW);
+
+	/*
+	 * XXX: Bitwise-inclusive ORs of all combinations of the constants
+	 * shall result in distinct values.
+	 */
+
+	/* Non-negative values */
+	assert(FE_DOWNWARD >= 0);
+	assert(FE_TONEAREST >= 0);
+	assert(FE_TOWARDZERO >= 0);
+	assert(FE_UPWARD >= 0);
+
+	/* XXX: Distinct values */
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
