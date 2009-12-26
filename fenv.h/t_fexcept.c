d926194f9f2d518b3c90f372c3e2d552722cf8d1
stathis <stathis@opensolaris.(none)>
2009-12-26 06:13:04 +0200

    fenv.h: Initial import of t_fexcept.

---
+++ fenv.h/t_fexcept.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2009, Stathis Kamperis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <fenv.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma STDC FENV_ACCESS ON
+
+int
+main(void)
+{
+	int ex;
+
+	/* Clear all the floating-point exceptions. */
+	feclearexcept(FE_ALL_EXCEPT);
+
+	/* XXX: Perhaps add some math operations here? */
+
+	/* Make sure that no floating-point is set. */
+	ex = fetestexcept(FE_ALL_EXCEPT);
+	assert((ex & FE_ALL_EXCEPT) == 0);
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
