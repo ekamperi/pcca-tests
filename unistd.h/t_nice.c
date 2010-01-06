1adb7344069be65d1ac4cf38fbee53ca8452e66e
stathis <stathis@opensolaris.(none)>
2010-01-06 18:40:08 +0200

    unistd.h/t_nice: Cleanup.

--- unistd.h/t_nice.c
+++ unistd.h/t_nice.c
@@ -25,9 +25,10 @@
  * SUCH DAMAGE.
  */
 
+#define _XOPEN_SOURCE 600
+
 #include <assert.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -37,12 +38,19 @@ main(void)
 {
 	int rv, rv2;
 
-        /*
+	/* Make sure we don't run the test as root. */
+	if (getuid() == 0) {
+		fprintf(stderr, "FATAL: Test cases shouldn't be running "
+		    "as root user!\n");
+		assert(getuid() != 0);
+	}
+
+	/*
 	 * Try to lower our nice value (thus go to a more favorable scheduling),
 	 * without being privileged enough.
 	 */
-        rv = nice(-2);
-        assert(rv == -1 && errno == EPERM);
+	rv = nice(-2);
+	assert(rv == -1 && errno == EPERM);
 
 	/*
 	 * nice(3) may return -1 as a legitimate nice value.
