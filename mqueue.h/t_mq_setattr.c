3b54a39a78c924bcf1b38e536c7de6980aafaca2
Stathis Kamperis <ekamperi@gmail.com>
2010-01-16 03:32:40 +0200

    mqueue.h/t_mq_setattr: Cleanup and enhance.

--- mqueue.h/t_mq_setattr.c
+++ mqueue.h/t_mq_setattr.c
@@ -37,25 +37,54 @@
 
 int main(void)
 {
-	struct mq_attr attr, oldattr;
-	mqd_t md;
+	/* Invalid (or so we hope) message queue descriptor. */
+	struct mq_attr attr, curattr;
 
-	/* Invalid message queue descriptor. */
-	assert(mq_setattr(-1, &attr, NULL) == -1 && errno == EBADF);
+	assert(mq_setattr(-1, &attr, NULL    ) == -1 && errno == EBADF);
+	assert(mq_setattr(-1, &attr, &curattr) == -1 && errno == EBADF);
 
-	/* mq_setattr() and mq_getattr() must report the same exact values. */
+	/*
+	 * mq_getattr() must report the same exact values as those we provided
+	 * to mq_open() upon the queue creation.
+	 */
 	memset(&attr, 0, sizeof(attr));
-	attr.mq_maxmsg = 10;	/* Maximum number of messages. */
-	attr.mq_msgsize = 1024;	/* Maximum message size. */
+	attr.mq_maxmsg = 10;		/* Maximum number of messages. */
+	attr.mq_msgsize = 1024;		/* Maximum message size. */
+
+	mqd_t md;
 	md = mq_open(MQNAME, O_CREAT | O_EXCL | O_RDWR, 0700, &attr);
-	assert(md != -1);
+	assert(md != (mqd_t)-1);
 
-	assert(mq_getattr(md, &oldattr) != -1);
-	assert(oldattr.mq_maxmsg == attr.mq_maxmsg);
-	assert(oldattr.mq_msgsize == attr.mq_msgsize);
-	assert(oldattr.mq_curmsgs == 0 && attr.mq_curmsgs == 0);
+	assert(mq_getattr(md, &curattr) != -1);
+	assert(curattr.mq_maxmsg  == 10);
+	assert(curattr.mq_msgsize == 1024);
+	assert(curattr.mq_curmsgs == 0);
 
+	/*
+	 * Use mq_setattr() to set attributes.
+	 * POSIX says that the values of the mq_maxmsg, mq_msgsize, and
+	 * mq_curmsgs members of the mq_attr structure shall be ignored by
+	 * mq_setattr().
+	 */
+	struct mq_attr newattr, oldattr;
+
+	newattr.mq_maxmsg = 101;
+	newattr.mq_msgsize = 3000;
+	newattr.mq_curmsgs = 2;
+	assert(mq_setattr(md, &newattr, &oldattr) != -1);
+
+	assert(oldattr.mq_maxmsg  == 10);
+	assert(oldattr.mq_msgsize == 1024);
+	assert(oldattr.mq_curmsgs == 0);
+
+	assert(curattr.mq_maxmsg  == 10);
+	assert(curattr.mq_msgsize == 1024);
+	assert(curattr.mq_curmsgs == 0);
+
+	/* Disassociate message queue from system. */
 	assert(mq_close(md) != -1);
+
+	/* Remove message queue from system. */
 	assert(mq_unlink(MQNAME) != -1);
 
 	printf("passed\n");
