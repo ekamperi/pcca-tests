8e0b532e4e3ba370a4c8acee14b8e851bec8565a
Stathis Kamperis <ekamperi@gmail.com>
2010-01-21 13:31:22 +0200

    mqueue.h/t_mq_send3: Refactor to not use signals at all.

--- mqueue.h/t_mq_send3.c
+++ mqueue.h/t_mq_send3.c
@@ -50,8 +50,6 @@ main(void)
 
 	if (pid == 0) {
 		/* We are inside the child. */
-		/* Sleep a bit so that parent manages to setup the handler. */
-		assert(sleep(1) == 0);
 
 		/*
 		 * Send one more message than the maximum messages the queue
@@ -61,77 +59,42 @@ main(void)
 
 		for (i = 0; i < NMESSAGES + 1; i++) {
 			assert(mq_send(md, "foo", sizeof("foo"),
-				/* priority */ 0) != -1);
-
-			/*
-			 * Notify the parent that we have sent her a message, by
-			 * sending her a `SIGUSR1' signal.
-			 */
-			assert(kill(getppid(), SIGUSR1) != -1);
+				/* priority */ i + 1) != -1);
 		}
 	} else {
-		/* We are inside the parent. */
-		/* Setup signal handler. */
-		struct sigaction sa;
-
-		sa.sa_handler = intrhandler;
-		sa.sa_flags = 0;
-		assert(sigemptyset(&sa.sa_mask) != -1);
-		assert(sigaction(SIGUSR1, &sa, 0) != -1);
-
-		size_t i;
-
-		for (i = 0; i < NMESSAGES + 1; i++) {
-			/*
-			 * The value returned by sleep() is the `unslept'
-			 * amount of time.
-			 */
-			if (sleep(2) == 0) {
-				/*
-				 * If sleep() completed, child is most likely
-				 * being blocked on mq_send().
-				 */
-				break;
-			}
-		}
-
-		/* If child never blocked, it's a failure. */
-		assert(i < NMESSAGES);
+		/*
+		 * We are inside the parent.
+		 *
+		 * Sleep a bit so that child takes its time to block on
+		 * mq_send().
+		 */
+		assert(sleep(2) == 0);
 
 		/*
-		 * We drain a message from the queue.
-		 * This should normally cause the child that is blocked on
-		 * mq_send() to unblock and complete.
+		 * Start draining messages from the queue.
+		 * Messages are sent with increasing priority, so the last sent
+		 * is the first to be delivered.
 		 */
 		char buf[1024];
 		int rv;
 		unsigned int prio;
 
-		rv = mq_receive(md, buf, sizeof(buf), &prio);
-		if (rv == -1) {
-			/* Kill child (which is blocked). */
-			assert(kill(pid, SIGKILL) != -1);
+		/*
+		 * 10th message --
+		 * this means that the last mq_send() call blocked.
+		 */
+		assert(mq_receive(md, buf, sizeof(buf), &prio) != -1);
+		assert(prio == NMESSAGES);
 
-			/* Probe its status to make sure it's gone. */
-			int status;
-			assert(wait(&status) == pid);
-
-			/* Abort. */
-			assert(rv != -1);
-		}
+		assert(sleep(1) == 0);
 
 		/*
-		 * At this point the child has been unblocked and sent us the
-		 * last message along with the last `SIGUSR1' signal.
+		 * 11th message --
+		 * this means that the last mq_send() call unblocked as it ought
+		 * to.
 		 */
-		rv = sleep(2);
-		if (rv == 0) {
-			/* Kill child. */
-			assert(kill(pid, SIGKILL) != -1);
-
-			/* Abort. */
-			assert(rv != 0);
-		}
+		assert(mq_receive(md, buf, sizeof(buf), &prio) != -1);
+		assert(prio == NMESSAGES + 1);
 
 		/* We are done -- cleanup */
 		assert(mq_close(md) != -1);
@@ -142,15 +105,6 @@ main(void)
 		return (EXIT_SUCCESS);
 	}
 
-	/* Only reached by child. */
+	/* Only reached by the child. */
 	return (EXIT_SUCCESS);
-}
-
-static void
-intrhandler(int signo)
-{
-	/* To keep compiler happy. */
-	(void)signo;
-
-	return;
 }
