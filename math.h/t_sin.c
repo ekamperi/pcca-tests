5279a9cae96528696345eb7b8496beac6b4d18d1
stathis <stathis@opensolaris.(none)>
2010-01-22 18:32:35 +0200

    math.h/t_sin: Fix typos from c|p.

--- math.h/t_sin.c
+++ math.h/t_sin.c
@@ -5,7 +5,7 @@
 
 struct tentry {
 	double x;       /* Input */
-	double y;       /* rint output */
+	double y;       /* sin output */
 } ttable[] = {
 	/* If x is +/-0, x shall be returned. */
 	{ +0.0, +0.0 },
@@ -17,7 +17,7 @@ int main(void)
 	size_t i;
 
 	for (i = 0; i < sizeof(ttable) / sizeof(struct tentry); i++)
-		assert(fabs(rint(ttable[i].x) - ttable[i].y) < 0.001);
+		assert(fabs(sin(ttable[i].x) - ttable[i].y) < 0.001);
 
 	/* Ix x is NaN, a NaN shall be returned. */
 	assert(isnan(sin(nan(""))) != 0);
