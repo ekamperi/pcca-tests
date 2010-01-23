360cd2606448a77409415f3191104c4c1a1cdd1b
stathis <stathis@opensolaris.(none)>
2010-01-23 02:51:15 +0200

    Initial import of t_cos.

---
+++ math.h/t_cos.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2009, Stathis Kamperis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tentry {
+	double x;       /* Input */
+	double y;       /* cos output */
+} ttable[] = {
+	/* If x is +/-0, x shall be returned. */
+	{ +0.0, +0.0 },
+	{ -0.0, -0.0 }
+
+	/* XXX: To be populated. */
+};
+
+int
+main(void)
+{
+	size_t i, N;
+
+	N = sizeof(ttable) / sizeof(struct tentry);
+	for (i = 0; i < N; i++)
+		assert(fabs(cos(ttable[i].x) - ttable[i].y) < 0.001);
+
+	/* Ix x is NaN, a NaN shall be returned. */
+	assert(isnan(sin(nan(""))) != 0);
+
+	printf("passed\n");
+
+	return (EXIT_SUCCESS);
+}
