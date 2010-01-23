2a81d0e6f5610f88e15585c6e059c06ee00b3b3d
stathis <stathis@opensolaris.(none)>
2010-01-23 02:39:19 +0200

    math.h/t_sin: Make it actually look like a test case.

--- math.h/t_sin.c
+++ math.h/t_sin.c
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2009, Stathis Kamperis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
@@ -8,15 +35,27 @@ struct tentry {
 	double y;       /* sin output */
 } ttable[] = {
 	/* If x is +/-0, x shall be returned. */
-	{ +0.0, +0.0 },
-	{ -0.0, -0.0 }
+	{   +0.0,	+0.0	 },
+	{   -0.0,	-0.0	 },
+
+	{  -19.902800, -0.869032 },
+	{  -16.642800,  0.804502 },
+	{   -9.382800, -0.041965 },
+	{    1.877199,  0.953424 },
+	{   17.137200, -0.989997 },
+	{   36.397200, -0.964067 },
+	{   59.657200,  0.033054 },
+	{  86.9172000, -0.866123 },
+	{ 118.1772000, -0.933237 }
 };
 
-int main(void)
+int
+main(void)
 {
-	size_t i;
+	size_t i, N;
 
-	for (i = 0; i < sizeof(ttable) / sizeof(struct tentry); i++)
+	N = sizeof(ttable) / sizeof(struct tentry);
+	for (i = 0; i < N; i++)
 		assert(fabs(sin(ttable[i].x) - ttable[i].y) < 0.001);
 
 	/* Ix x is NaN, a NaN shall be returned. */
