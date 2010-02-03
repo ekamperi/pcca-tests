83e50b84d344c8ce5abe37613abd6556a4eb5fbe
beket <beket@voyager.(none)>
2010-02-03 17:08:44 +0200

    Import getkilled.sh

---
+++ getkilled.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Exit immediately if any untested command fails in non-interactive mode.
+set -e
+
+# Unless explicitly provided, use current directory as the default one.
+if [ ! -z "$1" ]; then
+    STARTDIR="$1"
+else
+    STARTDIR="."
+fi
+
+find "$STARTDIR" -name .git -prune -o \( -type f -name "log.killed" -exec \
+    cat {} \; \) 2>/dev/null | sort
