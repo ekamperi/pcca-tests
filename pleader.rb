6991cf5098ecfc269f79ab9dfaf926f6c67ab685
stathis <stathis@opensolaris.(none)>
2010-02-06 20:24:24 +0200

    pleader.rb: Log.

--- pleader.rb
+++ pleader.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/ruby
 
+log_passed = File.open("#{ARGV[0]}/log.passed", "w+")
+log_failed = File.open("#{ARGV[0]}/log.failed", "w+")
+
 tests = Dir.glob("#{ARGV[0]}/t_*_prot.c")
 
 tests.each { |t|
@@ -11,7 +14,12 @@ tests.each { |t|
         print "#{t}: "
         if ($?.exitstatus == 0)
                 puts "passed"
+                log_passed.puts "#{File.basename(t, '.c')}"
         else
                 puts "failed"
+                log_failed.puts "#{File.basename(t, '.c')}"
         end
 }
+
+log_passed.close
+log_failed.close
