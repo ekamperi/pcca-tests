cff9115c76f03ca4fe4389c28fd4d52f51fcec77
Stathis Kamperis <ekamperi@gmail.com>
2010-02-07 20:57:34 +0200

    dbpop: Cosolidate addheaderfuncsh + addallfuncs scripts.

--- dbpop/addallfuncs.sh
+++ dbpop/addallfuncs.sh
@@ -10,5 +10,31 @@ else
     STARTDIR=".."
 fi
 
-find "$STARTDIR" -name .git -prune -o \( -type d -name "*.h" -exec \
-    ./addheaderfuncs.sh {}/functions.list \; \) 2>/dev/null | sort
+# For every given `functions.list' file, generate a set of INSERT records
+# to populate the SQL database.
+popfunctions()
+{
+    # First line is always the header.
+    {
+	read header
+	while read func; do
+	    # Skip empty lines.
+	    if [ -z "$func" ]; then
+		continue
+	    fi
+
+	    printf "INSERT INTO psx_functions VALUES('$header', '$func', 'pending',"
+	    printf " 'pending', 'pending', 'pending', 'pending', 'pending', 'pending');\n"
+	done
+    } < "$1"
+}
+
+# Scan all $header/ directories.
+for dir in $(find "$STARTDIR" -name .git -prune -o \
+    \( -type d -name "*.h" -print \) 2>/dev/null | sort)
+do
+    if [ -f "$dir/functions.list" ]
+    then
+	popfunctions "$dir/functions.list"
+    fi
+done
