61cc9ea24816bfaf36b75776b6425fb143663c77
stathis <stathis@opensolaris.(none)>
2010-02-07 18:59:02 +0200

    protgen.sh: Don't assume . as start dir

--- protgen.sh
+++ protgen.sh
@@ -3,8 +3,14 @@
 # Exit immediately if any untested command fails in non-interactive mode.
 set -e
 
-for dir in $(find . -name .git -prune -o \( -type d -name "*.h" -print \) \
-    2>/dev/null | sort)
+if [ ! -z "$1" ]; then
+    STARTDIR="$1"
+else
+    STARTDIR="."    # Default top level directory to start from.
+fi
+
+for dir in $(find "$STARTDIR" -name .git -prune -o \
+    \( -type d -name "*.h" -print \) 2>/dev/null | sort)
 do
     if [ -f "$dir/prototypes.list" ]
     then
