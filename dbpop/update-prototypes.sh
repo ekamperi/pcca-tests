0af8b82743d624eac7c65bff1c12b031b2d5c379
stathis <stathis@opensolaris.(none)>
2010-02-08 20:04:52 +0200

    Import update-prototypes.sh :)

---
+++ dbpop/update-prototypes.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Exit immediately if any untested command fails in non-interactive mode.
+set -e
+
+# Unless explicitly provided, use current directory as the default one.
+if [ ! -z "$1" ]; then
+    STARTDIR="$1"
+else
+    STARTDIR=".."
+fi
+
+# First the prototypes tests that passed
+for prot in $(find "$STARTDIR" -name .git -prune -o \
+    \( -type f -name "log.passed-prototypes" -exec cat {} \; \) 2>/dev/null | sort)
+do
+    func=$(echo "$prot" | awk -F '^(t_)|(_prot)$' ' { print $2 }')
+    echo "UPDATE psx_functions SET prototype = 'yes'  WHERE function_name = '$func';"
+done
+
+# First the prototypes tests that failed
+for prot in $(find "$STARTDIR" -name .git -prune -o \
+    \( -type f -name "log.failed-prototypes" -exec cat {} \; \) 2>/dev/null | sort)
+do
+    func=$(echo "$prot" | awk -F '^(t_)|(_prot)$' ' { print $2 }')
+    echo "UPDATE psx_functions SET prototype = 'no'  WHERE function_name = '$func';"
+done
