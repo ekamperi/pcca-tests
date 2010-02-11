0f950abb797a085c8fbc24eb7905d94678dd4bcf
Stathis Kamperis <ekamperi@gmail.com>
2010-02-12 01:14:10 +0200

    extract-functions.sh: Cleanup pass.

--- etc/extract-functions.sh
+++ etc/extract-functions.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
+#
+# This script iterates over the entire test suite. For every functional test
+# case it encounters, it extracts the functions that are called from within
+# that particular test.
+#
+# The output is saved in a file named exactly as the test source file, with the
+# .cflow string appended to it. E.g., t_mq_open.c will give birth to
+# t_mq_open.c.cflow
 
 # Exit immediately if any untested command fails in non-interactive mode.
-#set -e
+set -e
 
 if [ ! -z "$1" ]; then
     STARTDIR="$1"
 else
-    STARTDIR="."    # Default top level directory to start from.
+    STARTDIR=".."    # Default top level directory to start from.
 fi
 
 # Note: Skip the prototype tests, as they don't audit functionality.
@@ -23,13 +31,15 @@ do
     # But skip assert(), printf() and anything that contains < > symbols.
     cflow "$file" | egrep -v '<|>|assert|printf' | sort > "$file.cflow"
 
-    # Remove leading whitespace.
+    # Remove leading whitespace and trailing () from every function/line.
     # Note: Don't be tempted to use the more common, but severy less portable,
-    # ^[ \t] construct. Nor use sed -i.
+    # ^[ \t] construct. Nor use sed -i. They are both gnu-isms and will break
+    # to bsdland.
     sed 's/^[[:blank:]]\{1,\}\(.*\)()$/\1/g' "$file.cflow" > "$file.cflow.tmp"
     mv "$file.cflow.tmp" "$file.cflow"
     rm -f "$file.cflow.tmp"
 
+    # Debugging purposes
     echo "$file"
     cat "$file.cflow"
 done
