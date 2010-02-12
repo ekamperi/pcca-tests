25a8ed3852f59503e399b8ac1a893eea6146218b
Stathis Kamperis <ekamperi@gmail.com>
2010-02-12 20:39:08 +0200

    dbpop/get-tests-for-function.rb: Scan all tfile.xml files.

--- dbpop/get-tests-for-function.rb
+++ dbpop/get-tests-for-function.rb
@@ -6,19 +6,31 @@
 # The scipt parses the XML file and returns the names of those test cases that
 # use the audited function.
 
+require 'find'
 require 'rexml/document'
 include REXML
 
-xmlfile = File.new(ARGV[0], "r")
-doc = Document.new(xmlfile)
+def queryxml(xmlfile, function)
+    f = File.new(xmlfile, "r")
+    doc = Document.new(f)
 
-XPath.each(doc.root,
-           '//test/calls/function[text()=$function]',
-           {},
-           {"function"=>"#{ARGV[1]}"}) { |e|
-    test = e.parent.parent
-    binary = XPath.first(test, 'binary')
-    puts binary.text
+    XPath.each(doc.root,
+               "//test/calls/function[text()='#{function}']") { |e|
+        test = e.parent.parent
+        binary = XPath.first(test, 'binary')
+        puts binary.text
+    }
+
+    f.close
+end
+
+Find.find("../") { |p|
+    # Skip .git
+    if FileTest.directory?(p) && File.basename(p) == ".git"
+        Find.prune
+    else
+        if File.basename(p) == "tfile.xml"
+            queryxml(p, "#{ARGV[0]}")
+        end
+    end
 }
-
-xmlfile.close
