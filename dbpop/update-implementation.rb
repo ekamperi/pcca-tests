a8b5342aa80bb3c26c722e8bd188711ca067476a
Stathis Kamperis <ekamperi@gmail.com>
2010-02-13 03:09:09 +0200

    dbpop/update-implementation.rb: Now it is finished!
    But it is superslow :( I didn't expect to be fast, given
    the many file system traversals. But that slow ?
    
    XXX: Needs to be revisited.

--- dbpop/update-implementation.rb
+++ dbpop/update-implementation.rb
@@ -14,27 +14,57 @@ def queryxml(xmlfile, function)
         f = File.new(xmlfile, "r")
         doc = Document.new(f)
 
+        binaries = []
         XPath.each(doc.root,
                    "//test/calls/function[text()='#{function}']") { |e|
                 test = e.parent.parent
                 binary = XPath.first(test, 'binary')
-                puts binary.text
+                binaries << [File.dirname(xmlfile), binary.text]
         }
 
         f.close
+
+        return binaries
 end
 
 def queryfunction(function)
+        binaries = []
         Find.find("../") { |p|
                 # Skip .git
                 if FileTest.directory?(p) && File.basename(p) == ".git"
                         Find.prune
                 else
                         if File.basename(p) == "tfile.xml"
-                                queryxml(p, function)
+                                tmp = queryxml(p, function)
+                                binaries << tmp if tmp != []
                         end
                 end
         }
+
+        return binaries
+end
+
+def querytests(binaries)
+        # There are no test cases -- inconclusive
+        return 0 if binaries.empty?
+
+        # If there are test cases, and _at least_ one of them managed to
+        # _build_, we assume the function is implemented.
+        binaries[0].each { |b|
+                f = File.new("#{b[0]}/log.passed", 'r')
+                while (line = f.gets)
+                        return 1 if line.include? b[1]
+                end
+                f.close
+
+                f = File.new("#{b[0]}/log.failed-runtime", 'r')
+                while (line = f.gets)
+                        return 1 if line.include? b[1]
+                end
+                f.close
+        }
+
+        return -1
 end
 
 Find.find("../") { |p|
@@ -49,8 +79,16 @@ Find.find("../") { |p|
 
                                 next if line == ""
                                 next if line =~ /\.h$/
-                                puts ">>> #{line}"
-                                queryfunction(line)
+                                #puts ">>> #{line}"
+                                rv = querytests(queryfunction(line))
+                                case rv
+                                when 1
+                                        puts "UPDATE psx_functions SET implemented = 'yes' WHERE function_name '#{line}'"
+                                when -1
+                                        puts "UPDATE psx_functions SET implemented = 'no' WHERE function_name '#{line}'"
+                                when 0
+                                        puts "Pending"
+                                end
                         end
                         f.close
                 end
