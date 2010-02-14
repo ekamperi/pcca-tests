2f82d52a737f0af10a6b5bd290a6b0f38dfd2fb9
stathis <stathis@opensolaris.(none)>
2010-02-14 08:39:24 +0200

    aio.h/t_aio: Cleanup.

--- aio.h/t_aio.c
+++ aio.h/t_aio.c
@@ -32,10 +32,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
 	char mybuf[1000];
 	struct aiocb iocb;
