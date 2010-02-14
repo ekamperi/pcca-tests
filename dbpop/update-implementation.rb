d860067a4e4968279bd2ad9bb1b80063db4766a2
stathis <stathis@opensolaris.(none)>
2010-02-15 00:01:59 +0200

    dbpop/update-implementation.rb: Comments.

--- dbpop/update-implementation.rb
+++ dbpop/update-implementation.rb
@@ -1,15 +1,33 @@
 #!/usr/pkg/bin/ruby
 #
-# ARGV[0] is the path the tfile.xml file
-# ARGV[1] is the name of the function
+# This script generates SQL statements of the form:
+# UPDATE psx_functions SET implemented = 'xxx' WHERE function_name = 'yyy';#
 #
-# The scipt parses the XML file and returns the names of those test cases that
-# use the audited function.
+# It does so, for every POSIX function as found in the various per-header
+# `function.list' files.
+#
+# In order to determine whether a function is implemented or not, we search
+# across the entire repository for any possible tests that use it.
+#     - If there's none, we simply delegate the decision as 'pending'.
+#
+#     - If there's at least one (possibly out of many), that succeeds at
+#       building, linking and running, we mark the function as implemented.
+#
+#     - If there's none that succeeds and there are at least 2 tests,
+#       we consider that the common failing denominator is the function.
+#       This can of course, lead to false positive results. Increasing the
+#       threshold to 3 or more, will eventually make the problem go away.
+#
 
 require 'rubygems'
 require 'find'
 require 'nokogiri'
 
+#
+# Extract all test cases from `xmlfile', that use `function'.
+# The results are return as an array of the form:
+# [['../mqueue.h', 't_mq_open'], ...]
+#
 def queryxml(xmlfile, function)
         f = File.new(xmlfile, "r")
         doc = Nokogiri::XML(f)
@@ -26,6 +44,10 @@ def queryxml(xmlfile, function)
         return binaries
 end
 
+#
+# Search across the whole repository (i.e., across all `tfile.xml' files) for
+# test that use `function'.
+#
 def queryfunction(function)
         binaries = []
         Find.find("../") { |p|
@@ -43,12 +65,16 @@ def queryfunction(function)
         return binaries
 end
 
+#
+# For all the tests that we have gathered (if any), check their build, link, run
+# outcome and decide accordingly whether the function is implemented or not.
+#
 def querytests(binaries)
-        # There are no test cases -- inconclusive
+        # There are no test cases -- inconclusive.
         return 0 if binaries.empty?
 
         # If there are test cases, and _at least_ one of them managed to
-        # _build_, we assume the function is implemented.
+        # _pass_, we assume the function is implemented.
         binaries.each { |b|
                 f = File.new("#{b[0]}/log.passed", 'r')
                 while (line = f.gets)
@@ -58,8 +84,10 @@ def querytests(binaries)
         }
 
         # Require at least 2 failing test cases.
+        # XXX: Increase this to eliminate false positive results.
         return 0 if binaries.size < 2
 
+        # The function is not implemented.
         return -1
 end
 
@@ -83,7 +111,7 @@ Find.find("../") { |p|
                                 when -1
                                         puts "UPDATE psx_functions SET implemented = 'no'  WHERE function_name = '#{line}';"
                                 when 0
-                                    puts "UPDATE psx_functions SET implemented = 'pending' WHERE function_name = '#{line}';"
+                                        puts "UPDATE psx_functions SET implemented = 'pending' WHERE function_name = '#{line}';"
                                 end
                                 STDOUT.flush
                         end
