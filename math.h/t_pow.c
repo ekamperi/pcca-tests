8d35f2bddbc2f64479fe9daff249e49366b1333e
stathis <stathis@voyager.localdomain>
2010-03-02 16:46:49 +0200

    math.h/t_pow: Add copyright, plus some checks.

--- math.h/t_pow.c
+++ math.h/t_pow.c
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2010, Stathis Kamperis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
@@ -149,11 +176,13 @@ main(void)
 
 	N = sizeof(ttable) / sizeof(struct tentry);
 	for (i = 0; i < N; i++) {
-		/* Paranoia */
+		/* Make sure that only allowed checks are set */
 		assert((ttable[i].exchk & ~(CHK_REG | CHK_SIGN | CHK_INF)) == 0);
 
-		oval = pow(ttable[i].x, ttable[i].y);
+		/* Don't allow conflicting types to be set */
+		assert((ttable[i].exchk & (CHK_REG | CHK_INF)) != (CHK_REG | CHK_INF));
 
+		oval = pow(ttable[i].x, ttable[i].y);
 		if (ttable[i].exchk & CHK_REG) {
 			assert(fabs(oval - ttable[i].z) < 0.001);
 		}
