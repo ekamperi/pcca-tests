fece4ee937ef62cf41d1edefbf11ce845d33e0d9
Stathis Kamperis <ekamperi@gmail.com>
2010-03-03 10:36:48 +0200

    math.h/t_pow: More checks re zero, plus cleanup.

--- math.h/t_pow.c
+++ math.h/t_pow.c
@@ -31,8 +31,9 @@
 #include <stdlib.h>
 
 #define	CHK_REG		(1 << 0)
-#define CHK_INF		(1 << 1)
-#define CHK_SIGN	(1 << 2)
+#define CHK_ZERO	(1 << 1)
+#define CHK_INF		(1 << 2)
+#define CHK_SIGN	(1 << 3)
 
 struct tentry {
 	double x;       /* Input */
@@ -65,80 +66,80 @@ struct tentry {
 	{  -1, +0.0, 1.0, CHK_REG },
 	{   0, +0.0, 1.0, CHK_REG },
 	{ NAN, +0.0, 1.0, CHK_REG },
-        {   1, -0.0, 1.0, CHK_REG },
-        {  -1, -0.0, 1.0, CHK_REG },
-        {   0, -0.0, 1.0, CHK_REG },
-        { NAN, -0.0, 1.0, CHK_REG },
+	{   1, -0.0, 1.0, CHK_REG },
+	{  -1, -0.0, 1.0, CHK_REG },
+	{   0, -0.0, 1.0, CHK_REG },
+	{ NAN, -0.0, 1.0, CHK_REG },
 
 	/*
 	 * For any odd integer value of y > 0, if x is +-0,
 	 * +-0 shall be returned.
 	 */
-	{ +0.0, 1, +0.0, CHK_REG | CHK_SIGN },
-	{ +0.0, 3, +0.0, CHK_REG | CHK_SIGN },
-        { -0.0, 1, -0.0, CHK_REG | CHK_SIGN },
-        { -0.0, 3, -0.0, CHK_REG | CHK_SIGN },
+	{ +0.0, 1, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ +0.0, 3, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ -0.0, 1, -0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ -0.0, 3, -0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
 
 	/*
 	 * For y > 0 and not an odd integer, if x is +-0,
 	 * +0 shall be returned.
 	 */
-	{ +0.0, 2, +0.0, CHK_REG | CHK_SIGN },
-        { +0.0, 4, +0.0, CHK_REG | CHK_SIGN },
-        { -0.0, 2, +0.0, CHK_REG | CHK_SIGN },
-        { -0.0, 4, +0.0, CHK_REG | CHK_SIGN },
+	{ +0.0, 2, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ +0.0, 4, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ -0.0, 2, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ -0.0, 4, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
 
 	/* If x is -1, and y is +-Inf, 1.0 shall be returned. */
 	{ -1, +INFINITY, 1.0, CHK_REG },
-        { -1, +INFINITY, 1.0, CHK_REG },
-        { -1, -INFINITY, 1.0, CHK_REG },
-        { -1, -INFINITY, 1.0, CHK_REG },
+	{ -1, +INFINITY, 1.0, CHK_REG },
+	{ -1, -INFINITY, 1.0, CHK_REG },
+	{ -1, -INFINITY, 1.0, CHK_REG },
 
 	/* For |x| < 1, if y is -Inf, +Inf shall be returned. */
 	{ +0.0, -INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
-        { -0.0, -INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
-        { +0.5, -INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
+	{ -0.0, -INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
+	{ +0.5, -INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
 	{ -0.5, -INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
 
 	/* For |x| > 1, if y is -Inf, +0 shall be returned. */
-        { +1.1, -INFINITY, +0.0, CHK_REG | CHK_SIGN },
-        { -1.1, -INFINITY, +0.0, CHK_REG | CHK_SIGN },
-        { +2.0, -INFINITY, +0.0, CHK_REG | CHK_SIGN },
-        { -2.0, -INFINITY, +0.0, CHK_REG | CHK_SIGN },
+	{ +1.1, -INFINITY, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ -1.1, -INFINITY, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ +2.0, -INFINITY, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ -2.0, -INFINITY, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
 
 	/* For |x| < 1, if y is +Inf, +0 shall be returned. */
-	{ +0.0, +INFINITY, +0.0, CHK_REG | CHK_SIGN  },
-	{ -0.0, +INFINITY, +0.0, CHK_REG | CHK_SIGN  },
-        { +0.5, +INFINITY, +0.0, CHK_REG | CHK_SIGN  },
-        { -0.5, +INFINITY, +0.0, CHK_REG | CHK_SIGN  },
+	{ +0.0, +INFINITY, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN  },
+	{ -0.0, +INFINITY, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN  },
+	{ +0.5, +INFINITY, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN  },
+	{ -0.5, +INFINITY, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN  },
 
 	/* For |x| > 1, if y is +Inf, +Inf shall be returned. */
 	{ +1.1, +INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
 	{ -1.1, +INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
-        { +2.0, +INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
+	{ +2.0, +INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
 	{ -2.0, +INFINITY, +INFINITY, CHK_INF | CHK_SIGN },
 
 
 	/* For y an odd integer < 0, if x is -Inf, -0 shall be returned. */
 	{ -INFINITY, -1, -0.0, CHK_REG | CHK_SIGN },
-        { -INFINITY, -3, -0.0, CHK_REG | CHK_SIGN },
-        { -INFINITY, -5, -0.0, CHK_REG | CHK_SIGN },
-        { -INFINITY, -7, -0.0, CHK_REG | CHK_SIGN },
+	{ -INFINITY, -3, -0.0, CHK_REG | CHK_SIGN },
+	{ -INFINITY, -5, -0.0, CHK_REG | CHK_SIGN },
+	{ -INFINITY, -7, -0.0, CHK_REG | CHK_SIGN },
 
 	/*
 	 * For y < 0 and not an odd integer, if x is -Inf,
 	 * +0 shall be returned.
 	 */
-	{ -INFINITY, -2, +0.0, CHK_REG | CHK_SIGN },
-        { -INFINITY, -4, +0.0, CHK_REG | CHK_SIGN },
-        { -INFINITY, -6, +0.0, CHK_REG | CHK_SIGN },
-        { -INFINITY, -8, +0.0, CHK_REG | CHK_SIGN },
+	{ -INFINITY, -2, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ -INFINITY, -4, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ -INFINITY, -6, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ -INFINITY, -8, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
 
 	/* For y an odd integer > 0, if x is -Inf, -Inf shall be returned. */
 	{ -INFINITY, 1, -INFINITY, CHK_INF | CHK_SIGN },
 	{ -INFINITY, 3, -INFINITY, CHK_INF | CHK_SIGN },
-        { -INFINITY, 5, -INFINITY, CHK_INF | CHK_SIGN },
-        { -INFINITY, 7, -INFINITY, CHK_INF | CHK_SIGN },
+	{ -INFINITY, 5, -INFINITY, CHK_INF | CHK_SIGN },
+	{ -INFINITY, 7, -INFINITY, CHK_INF | CHK_SIGN },
 
 	/*
 	 * For y > 0 and not an odd integer, if x is -Inf,
@@ -146,40 +147,40 @@ struct tentry {
 	 */
 	{ -INFINITY, 2, +INFINITY, CHK_INF | CHK_SIGN },
 	{ -INFINITY, 4, +INFINITY, CHK_INF | CHK_SIGN },
-        { -INFINITY, 6, +INFINITY, CHK_INF | CHK_SIGN },
-        { -INFINITY, 8, +INFINITY, CHK_INF | CHK_SIGN },
+	{ -INFINITY, 6, +INFINITY, CHK_INF | CHK_SIGN },
+	{ -INFINITY, 8, +INFINITY, CHK_INF | CHK_SIGN },
 
 	/* For y < 0, if x is +Inf, +0 shall be returned. */
-        { +INFINITY, -1, +0.0, CHK_REG | CHK_SIGN },
-	{ +INFINITY, -2, +0.0, CHK_REG | CHK_SIGN },
-        { +INFINITY, -3, +0.0, CHK_REG | CHK_SIGN },
-	{ +INFINITY, -4, +0.0, CHK_REG | CHK_SIGN },
+	{ +INFINITY, -1, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ +INFINITY, -2, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ +INFINITY, -3, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
+	{ +INFINITY, -4, +0.0, CHK_REG | CHK_ZERO | CHK_SIGN },
 
 	/* For y > 0, if x is +Inf, +Inf shall be returned. */
-        { +INFINITY, 1, +INFINITY, CHK_INF | CHK_SIGN },
-        { +INFINITY, 2, +INFINITY, CHK_INF | CHK_SIGN },
-        { +INFINITY, 3, +INFINITY, CHK_INF | CHK_SIGN },
-        { +INFINITY, 4, +INFINITY, CHK_INF | CHK_SIGN },
+	{ +INFINITY, 1, +INFINITY, CHK_INF | CHK_SIGN },
+	{ +INFINITY, 2, +INFINITY, CHK_INF | CHK_SIGN },
+	{ +INFINITY, 3, +INFINITY, CHK_INF | CHK_SIGN },
+	{ +INFINITY, 4, +INFINITY, CHK_INF | CHK_SIGN },
 
 	/*
 	 * For y an odd integer < 0, if x is +-0, a pole error shall occur and
 	 * +-HUGE_VAL, +-HUGE_VALF, and +-HUGE_VALL shall be returned for pow(),
 	 * powf(), and powl(), respectively.
 	 */
-        { +0.0, -1, +HUGE_VAL, CHK_INF | CHK_SIGN },
-        { +0.0, -3, +HUGE_VAL, CHK_INF | CHK_SIGN },
-        { -0.0, -1, -HUGE_VAL, CHK_INF | CHK_SIGN },
-        { -0.0, -3, -HUGE_VAL, CHK_INF | CHK_SIGN },
+	{ +0.0, -1, +HUGE_VAL, CHK_INF | CHK_SIGN },
+	{ +0.0, -3, +HUGE_VAL, CHK_INF | CHK_SIGN },
+	{ -0.0, -1, -HUGE_VAL, CHK_INF | CHK_SIGN },
+	{ -0.0, -3, -HUGE_VAL, CHK_INF | CHK_SIGN },
 
 	/*
 	 * For y < 0 and not an odd integer, if x is +-0, a pole error shall
 	 * occur and HUGE_VAL, HUGE_VALF, and HUGE_VALL shall be returned for
 	 * pow(), powf(), and powl(), respectively.
 	 */
-        { +0.0, -2, HUGE_VAL, CHK_INF | CHK_SIGN },
-        { +0.0, -4, HUGE_VAL, CHK_INF | CHK_SIGN },
-        { -0.0, -2, HUGE_VAL, CHK_INF | CHK_SIGN },
-        { -0.0, -4, HUGE_VAL, CHK_INF | CHK_SIGN },
+	{ +0.0, -2, HUGE_VAL, CHK_INF | CHK_SIGN },
+	{ +0.0, -4, HUGE_VAL, CHK_INF | CHK_SIGN },
+	{ -0.0, -2, HUGE_VAL, CHK_INF | CHK_SIGN },
+	{ -0.0, -4, HUGE_VAL, CHK_INF | CHK_SIGN },
 };
 
 int
@@ -191,18 +192,23 @@ main(void)
 	N = sizeof(ttable) / sizeof(struct tentry);
 	for (i = 0; i < N; i++) {
 		/* Make sure that only allowed checks are set */
-		assert((ttable[i].check & ~(CHK_REG | CHK_SIGN | CHK_INF))
-		    == 0);
+		assert((ttable[i].check
+			& ~(CHK_REG | CHK_ZERO | CHK_INF | CHK_SIGN)) == 0);
 
 		/* Don't allow conflicting types to be set */
 		assert((ttable[i].check & (CHK_REG | CHK_INF))
 		    != (CHK_REG | CHK_INF));
+		assert((ttable[i].check & (CHK_ZERO | CHK_INF))
+		    != (CHK_ZERO | CHK_INF));
 
 		oval = pow(ttable[i].x, ttable[i].y);
 		if (ttable[i].check & CHK_REG) {
 			assert(fabs(oval - ttable[i].z) < 0.001);
 		}
-                if (ttable[i].check & CHK_INF) {
+		if (ttable[i].check & CHK_ZERO) {
+			assert(fpclassify(oval) == FP_ZERO);
+		}
+		if (ttable[i].check & CHK_INF) {
 			/*
 			 * Grab the opportunity to cross-check the results and
 			 * at the same time cross-check the functions with
